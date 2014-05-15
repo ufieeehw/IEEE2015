c7c48cccc89713ab9063afa2334d06dace54d677
marqueza <marquezaa@outlook.com>
2014-05-16 00:02:29 +0200

    Added basic rotation support for the nav sim.

--- ros/ieee2015_simulator/scripts/navigation_visualizer.py
+++ ros/ieee2015_simulator/scripts/navigation_visualizer.py
@@ -9,46 +9,69 @@ from geometry_msgs.msg import Twist
 width = 500
 height = 500
 bgcolor = 0,0,0
-hz = 10.0
+hz = 20.0
 ms = (int)(1000/hz)
 sec = ms / 1000.0
 
+
+#sufrace around the box
+navsurf = pygame.Surface((20, 20))
+navsurf.fill((200, 0, 0))
+
+#color key for blitting
+navsurf.set_colorkey((255, 0, 0))
+
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
+
 class Navigator:
-	box_color = 192, 192, 192
-	def __init__(self, x, y):
-		#set intial values
-		self.x = x
-		self.y = y
-		self.w = 10
-		self.h = 10
-		
-	def render(self, v):
-		#find new position based on update frequency
-		#sec = ms / 1000.0 seconds have passed since last update
-		dx = v.linear.x * sec
-		dy = v.linear.y * sec
-		print dx,dy
-		
-		#update position
-		self.x = self.x+dx
-		self.y = self.y+dy
-		
-		#clear screen and redraw the location of the robot
-		screen.fill(bgcolor)
-		pygame.draw.rect(screen, self.box_color, (self.x,self.y,self.w,self.h), 0 )
-		
-		pygame.display.flip()
-		#mas frames per second
+    box_color = 192, 192, 192
+
+    def __init__(self, x, y):
+        #set intial values
+        self.navbox = pygame.Rect((x, y, 20, 20))
+        self.degree = 1
+
+    def render(self, v):
+        #find new position based on update frequency
+        #sec = ms / 1000.0 seconds have passed since last update
+        dx = v.linear.x * sec
+        dy = v.linear.y * sec
+
+        #find new angle of orientation
+        dtheta = 3
+
+        #update position
+        self.navbox.x += dx
+        self.navbox.y += dy
+
+        #update orientation
+        self.degree += dtheta
+        if self.degree > 360:
+            self.degree -= 360
+
+        #clear screen
+        screen.fill(bgcolor)
+
+        #draw the location (base) of the robot
+        pygame.draw.rect(screen, self.box_color, self.navbox, 0 )
+
+        #rotate surf
+        rotatedSurf =  pygame.transform.rotate(navsurf, self.degree)
+
+        #get the rect of the rotated surf and set it's center to the base (navbox)
+        rotRect = rotatedSurf.get_rect()
+        rotRect.center = self.navbox.center
+        screen.blit(rotatedSurf, rotRect)
+
+        pygame.display.flip()
+
+        #mas frames per second
         clock.tick(240)
-		
-
-
 
 if __name__ == '__main__':
     #listener()
-    
+
     rospy.init_node('naviagtion_visualizer', anonymous=True)
     velocity = Twist()
     velocity.linear.x = 100
@@ -59,7 +82,7 @@ if __name__ == '__main__':
     velocity.angular.z = 0
     my_Navigator = Navigator(0, height/2)
     while not rospy.is_shutdown():
-		my_Navigator.render(velocity)
-		#delay by 100 miliseconds or 10 hz
-		pygame.time.wait(ms)
+        my_Navigator.render(velocity)
+        #delay by 100 miliseconds or 10 hz
+        pygame.time.wait(ms)
     rospy.spin()
