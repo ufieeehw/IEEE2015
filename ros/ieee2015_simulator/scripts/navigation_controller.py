7f5476615687f3b01193e9b3b49334230daf4661
marqueza <marquezaa@outlook.com>
2014-05-22 01:32:15 +0200

    Arrows keys move the box, 'a' and 'd' rotate the box

--- ros/ieee2015_simulator/scripts/navigation_controller.py
+++ ros/ieee2015_simulator/scripts/navigation_controller.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
 import rospy
+import pygame
 from geometry_msgs.msg import Twist
+
+#constants
+SCREEN_WIDTH = 200
+SCREEN_HEIGHT = 200
+bgcolor = 256,256,256
 
 def talker():
     pub = rospy.Publisher('navigation_control_signals', Twist)
     rospy.init_node('navigation_controller', anonymous=True)
     r = rospy.Rate(10) # 10hz
+    velocity = Twist()
+    velocity.linear.x = 0
+    velocity.linear.y = 0
+    velocity.linear.z = 0
+    velocity.angular.x = 0
+    velocity.angular.y = 0
+    velocity.angular.z = 0
+    increment = 20
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while not rospy.is_shutdown():
-        
-        #to be replaced with more robust code
-        velocity = Twist()
-        velocity.linear.x = 100
-        velocity.linear.y = 0
-        velocity.linear.z = 0
-        velocity.angular.x = 0
-        velocity.angular.y = 0
-        velocity.angular.z = 100
-        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    velocity.linear.x += increment
+                if event.key == pygame.K_LEFT:
+                    velocity.linear.x -= increment
+                if event.key == pygame.K_UP:
+                    velocity.linear.y -= increment
+                if event.key == pygame.K_DOWN:
+                    velocity.linear.y += increment
+                if event.key == pygame.K_a:
+                    velocity.angular.z -= increment
+                if event.key == pygame.K_d:
+                    velocity.angular.z += increment
+    
         pub.publish(velocity)
         r.sleep()
 
