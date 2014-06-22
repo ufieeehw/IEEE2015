3f65103dc4ae07f00917c05c329c1cb932cc0cf8
Jacob Panikulam <jpanikulam@ufl.edu>
2014-06-22 16:54:13 -0400

    Working arm-angle simulator

--- ros/ieee2015_simulator/scripts/arm_simulator.py
+++ ros/ieee2015_simulator/scripts/arm_simulator.py
@@ -15,8 +15,17 @@ from geometry_msgs.msg import Point, PoseStamped, Pose, Quaternion
 SCREEN_DIM = (500,500)
 ORIGIN = np.array([SCREEN_DIM[0]/2.0, SCREEN_DIM[1]/2.0])
 
+def print_in(f):
+    print("Defining " + f.func_name)
+    def print_on_entry(*args, **kwargs):
+        print("Executing " + f.func_name + " with arguments " + str(args) + " and kwargs " + str(kwargs))
+        result = f(*args, **kwargs)
+        print("Returning " + str(result))
+        return(result)
+    return(print_on_entry)
+
 def round_point((x,y)):
-    return map(int, (x, -y) + ORIGIN)
+    return map(int, (x + ORIGIN[0], -y + ORIGIN[1]))
 
 def norm_angle_diff(ang_1, ang_2):
     '''norm_angle_diff(ang_1, ang_2)
@@ -26,27 +35,33 @@ def norm_angle_diff(ang_1, ang_2):
 
 
 class Line(object):
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
+    def __init__(self, point1, point2, color=(200,200,200)):
+        self.point1 = np.array(point1, np.float32)
+        self.point2 = np.array(point2, np.float32)
+        print "Initializing", self.point1, self.point2
+        self.color = color
     
     def update(self, point1, point2):
-        self.point1, self.point2 = point1, point2
+        self.point1 = np.array(point1, np.float32)
+        self.point2 = np.array(point2, np.float32)
     
     def draw(self, display):
-        pygame.draw.line(display, (255, 255, 255), round_point(self.point1), round_point(self.point2))
+        pygame.draw.line(display, self.color, round_point(self.point1), round_point(self.point2), 4)
     
     @property
     def end(self):
         '''Farthest point from origin'''
-        return max(self.points, key=lambda v: np.linalg.norm(v - (0,0)))
+        return np.array(max(self.points, key=lambda v: np.linalg.norm(np.array(v) - (0,0))))
     @property
     def start(self):
         '''Closest point to origin'''
-        return min(self.points, key=lambda v: np.linalg.norm(v - (0,0)))
+        return np.array(min(self.points, key=lambda v: np.linalg.norm(np.array(v) - (0,0))))
     @property
     def points(self):
-        return np.array([self.point1, self.point2], np.float32)
+        return [self.point1, self.point2]
+    @property
+    def norm(self):
+        return np.linalg.norm(np.array(self.point2)-np.array(self.point1))
 
     def __getitem__(self, key):
         return self.points[key]
@@ -65,49 +80,77 @@ class Line(object):
 class SCARA(object):
     def __init__(self):
         rospy.init_node('SCARA_simulator')
-        self.base = numpy.array([0.0, 0.0])
+        self.base = np.array([0.0, 0.0])
 
-        length1, length2 = 30, 20
-        self.joint1 = Line(np.array([0, 0]), (length1, 0))
-        self.joint2 = Line(self.joint1.end, self.joint1.end + (length2, 0))
+        length1, length2 = 100, 100
+        self.joint1 = Line(self.base, (length1, 0), color=(100,100,100))
+        self.joint1.end
+        self.joint2 = Line(self.joint1.end, self.joint1.end + (length2, 0), color=(200,200,100))
 
-        self.angle1, self.angle2 = 0.0,0.0
+        self.elbow_sub = rospy.Subscriber('arm_elbow_angle', Float32, self.got_elbow_angle)
+        self.base_sub = rospy.Subscriber('arm_base_angle', Float32, self.got_base_angle)
 
-    def update(self):
-        self.new_end_1 = (np.tan(self.angle1))
+        self.error_sub = rospy.Subscriber('arm_des_pose', PoseStamped, self.got_des_pose)
+
+        self.angle1, self.angle2 = 0.0 , 0.0
+        self.des_pose = self.joint2[1]
+
+    def got_des_pose(self, msg):
+        self.position = np.array([msg.pose.position.x, msg.pose.position.y])
+
+    def got_elbow_angle(self, msg):
+        self.angle1 = msg.data
+
+    def got_base_angle(self, msg):
+        self.angle2 = msg.data
+
+    def update(self, center=(0,0)):
+        # TODO:
+        # Make this non-instantaneous
+
+        # Update elbow (end_1)
+        self.base = center
+
+        base_local_pos = self.joint1.norm * np.array([np.cos(self.angle1), np.sin(self.angle1)])
+        self.new_end_1 = base_local_pos + self.base
+
+        # Update endpoint as sum of base angle and elbow angle
+        total_elbow_angle = self.angle1 + self.angle2
+
+        # Superimpose positions
+        elbow_local_pos = self.joint2.norm * np.array([np.cos(total_elbow_angle), np.sin(total_elbow_angle)])
+        self.new_end_2 = self.new_end_1 + elbow_local_pos
 
         self.joint1.update(self.base, self.new_end_1)
-        self.joint2.update(self.joint1.end, new_end_2)
+        self.joint2.update(self.joint1.end, self.new_end_2)
 
-    def draw(self, display):
+    def draw(self, display, new_base=(0,0)):
+        self.update(new_base)
         self.joint1.draw(display)
         self.joint2.draw(display)
+
 
 
 def main():
     arm1 = SCARA()
     arms = [arm1]
 
-    print 'Joint 1 start', arm1.joint1.start
-    print 'Joint 1 end', arm1.joint1.end
-    print 'Joint 2 start', arm1.joint2.start
-    print 'Joint 2 end', arm1.joint2.end
-
     display = pygame.display.set_mode(SCREEN_DIM)
-    
+
     clock = pygame.time.Clock()
     while not rospy.is_shutdown():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         t = time.time()
         for arm in arms:
             arm.draw(display)
         
-        display.fill((0, 0, 0))
         pygame.display.update()
         clock.tick(60)
+        display.fill((0, 0, 0))
+
 
 if __name__ == '__main__':
     main()
