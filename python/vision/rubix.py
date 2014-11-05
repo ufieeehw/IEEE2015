1db20b0012dbb93611d7e8050cdf5f3c184bb0ca
JoshSandersUFgit <squash111@ufl.edu>
2014-11-04 22:43:21 -0500

    Vision: finished porting rubix detection

--- python/vision/rubix.py
+++ python/vision/rubix.py
@@ -6,31 +6,33 @@ import math
 #give the function a filename as an argument
 #returns the center and angle (point and float) or none
 def findRubix(filename):
-  src = imread(filename, IMREAD_COLOR) #get the image
-  if(!src.data):
+  src = cv2.imread(filename, cv2.IMREAD_COLOR) #get the image
+  if src == None:
     print 'Invalid Image'
     return None;  # no useful data
   
-	hsv_img = cvtColor(src, CV_BGR2HSV) #convert to HSV
-	fiter_img = inRange(hsv_img, array[0,150,100], array[180,255,255]) #filter all weak colors
-	blur_img = medianBlur(filter_img, 5) #blur the image
+  hsv_img = cv2.cvtColor(src, cv2.COLOR_BGR2HSV) #convert to HSV
+  filter_img = cv2.inRange(hsv_img, (0,150,100), (180,255,255)) #filter all weak colors
+  blur_img = cv2.medianBlur(filter_img, 5) #blur the image
   
-  contours = findContours(img_blur, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE) #get the contours
+  contours, hierarchy = cv2.findContours(blur_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #get the contours
   
-  rectList = [] # create list for rectangles
+  squares = [] # create list of the tile squares
   for c in contours:
-    rectList.append(minAreaRect(c)) #get a rectangle around the contour
+    squares.append(cv2.minAreaRect(c)) #get a rectangle around the contour
   
   # I'm realizing that extra contours might be an issue, will keep an eye on it
   
   #average the points
-  for i in range(len(rectList)):
-    angle = (angle*i + squares[i].angle/180*pi)/(i+1)  # convert to radians
-    x = (x*i + squares[i].center.x)/(i+1)
-    y = (y*i + squares[i].center.y)/(i+1)
+  angle = x = y = 0
+  print squares[0]
+  for i in range(len(squares)):
+    angle = (angle*i + squares[i][2]/180*math.pi)/(i+1)  # convert angle to radians
+    x = (x*i + squares[i][0][0])/(i+1)  # x coordinate of center
+    y = (y*i + squares[i][0][1])/(i+1)  # y coordinate of center
   
   # move origin from upper left to center of image
-  x = x - (img_blur.rows / 2)
-  y = (img_blur.cols / 2) - y
+  x = x - (len(blur_img) / 2)    #should be rows
+  y = (len(blur_img[0]) / 2) - y #should be cols
   
   return {'center':(x,y), 'angle':angle}
