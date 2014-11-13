bb4ee5eb5ae8c3768ad6068d43598d91eedab1ba
Pocathoughts <christinemoore321@gmail.com>
2014-11-13 02:24:44 -0500

    CHESS: Debugged rotate module

--- python/Chess/baseLine.py
+++ python/Chess/baseLine.py
@@ -11,8 +11,8 @@ def findAngle(filename):
 	edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
 	lines = cv2.HoughLines(edges,1,np.pi/180,200)
-
-	rho, theta = lines[0][0]
+	
+	rho, theta = lines[0][3]
 	a = np.cos(theta)
 	b = np.sin(theta)
 	x0 = a*rho
@@ -21,13 +21,29 @@ def findAngle(filename):
 	y1 = int(y0 + 1000*(a))
 	x2 = int(x0 - 1000*(-b))
 	y2 = int(y0 - 1000*(a))
-
-	if (theta > np.pi/4 and theta < np.pi*(3/4)):
-		angleOfRotation = theta - np.pi/2
-	elif (theta > 0 and theta < np.pi/4):
-		angleOfRotation = theta
-	else:
-		angleOfRotation = theta - np.pi
 	
+	
+	cv2.line(img, (x1, y1), (x2, y2), (255,0,0), 2)
+
+
+	#Determine which which direction and by how much the image should be rotated
+	if (theta > 0 and theta < np.pi/4.0):
+		angleOfRotation = theta
+
+	if (theta > np.pi/4.0 and theta < np.pi * (3/4.0)):
+		angleOfRotation = (theta - np.pi/2.0)
+
+	if (theta > np.pi * (3/4.0) and theta < np.pi * (5/4.0)):
+		angleOfRotation = (theta - np.pi/1.0)
+
+	if (theta > np.pi * (5/4.0) and theta < np.pi * (7/4.0)):
+		angleOfRotation = (theta - np.pi * (3/2.0))
+
+	if (theta > np.pi * (7/4.0)):
+		angleOfRotation = (theta - np.pi * 2.0)
+
+	else:
+		angleOfRotation = 0
+
 	return angleOfRotation*(180/np.pi)
 
