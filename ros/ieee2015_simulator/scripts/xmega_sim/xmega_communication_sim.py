b5a408499018608add8df14456a0737270afe976
zachgoins <zach.a.goins@gmail.com>
2014-11-22 02:00:16 -0500

    Xmega - Automated start for xmega sim

--- ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
+++ ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
@@ -1,23 +1,13 @@
 import subprocess
 import sys
 import serial
+import time
+import os
 
 # must install socat to operate
 # www.dest-unreach.org/socat/
 # or 
 # sudo apt-get install socat
-
-
-# subprocess to open fake TTy Portschomd 
-subprocess.call(["chmod", "u+x", "./com_ports_on.sh"])
-
-subprocess.Popen(["xterm","-hold", "-e", "./com_ports_on.sh"])
-
-#subprocess.call(["grome-terminal","-x", "sudo", "chmod", "666", "/dev/xmeega_tty"])
-
-
-
-
 
 print 
 print "Xmega simulation setup..."
@@ -25,14 +15,25 @@ print
 print "The proper PTS ports are now displayed in the popup terminal"
 print "Locate the port numbers and enter them now"
 
-#port_one = input('PTY Port 1: ') 
-port_two = input('PTY Port 2: ')
+
+# subprocess to open fake TTy Ports
+subprocess.call(["chmod", "u+x", "./com_ports_on.sh"])
+
+subprocess.Popen(["xterm", "-e", "./com_ports_on.sh"])
+
+time.sleep(.5)
+
+subprocess.call(["chmod", "666", "/dev/xmega_tty"])
+
+linked_path = os.path.realpath("/dev/xmega_tty")
+
+sliced_path = linked_path[9:] # returns pts number the tty port is linked to
 
 #convert_one = '/dev/pts/' + str(port_one)
-convert_two = '/dev/pts/' + str(port_two)
+convert_two = '/dev/pts/' + str(sliced_path)
 
 #print 'Streaming from ', convert_one
-print 'Streaming from' , convert_two
+print 'Waiting for signal from xmega driver at ' , convert_two
 
 #read_ser = serial.Serial(convert_one, 230400, timeout=1)
 read_ser_n = serial.Serial(convert_two)
