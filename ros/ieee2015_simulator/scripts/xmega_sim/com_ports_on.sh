454a3d88855f25919679b7828f8720e7cd9f711a
zachgoins <zach.a.goins@gmail.com>
2014-11-22 02:01:11 -0500

    Xmega - bash script for xmega_communication_sim.py

--- ros/ieee2015_simulator/scripts/xmega_sim/com_ports_on.sh
+++ ros/ieee2015_simulator/scripts/xmega_sim/com_ports_on.sh
@@ -5,9 +5,9 @@ echo
 echo
 echo "Ports will be closed when this window is closed"
 
-
 socat -d -d pty,link=/dev/ttyS30,echo=1 pty,link=/dev/xmega_tty,echo=1 
 
-gnome-terminal -x sudo chmod 666 /dev/xmega_tty
+
+ 
 
 
