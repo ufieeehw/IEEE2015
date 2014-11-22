bed0ff2e828c776bd905c54db17f8b600f31a08f
zachgoins <zach.a.goins@gmail.com>
2014-11-21 19:41:37 -0500

    Xmega - Impliment hex conversion for sim

--- ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
+++ ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
@@ -8,9 +8,16 @@ import serial
 # sudo apt-get install socat
 
 
-# subprocess to open fake COM Ports
+# subprocess to open fake TTy Portschomd 
 subprocess.call(["chmod", "u+x", "./com_ports_on.sh"])
+
 subprocess.Popen(["xterm","-hold", "-e", "./com_ports_on.sh"])
+
+#subprocess.call(["grome-terminal","-x", "sudo", "chmod", "666", "/dev/xmeega_tty"])
+
+
+
+
 
 print 
 print "Xmega simulation setup..."
@@ -18,25 +25,20 @@ print
 print "The proper PTS ports are now displayed in the popup terminal"
 print "Locate the port numbers and enter them now"
 
-port_one = input('PTY Port 1: ') 
-print 'Port one is ', port_one
+#port_one = input('PTY Port 1: ') 
 port_two = input('PTY Port 2: ')
-print 'Port two is ', port_two
-print port_one
 
-convert_one = '/dev/pts/' + str(port_one)
+#convert_one = '/dev/pts/' + str(port_one)
 convert_two = '/dev/pts/' + str(port_two)
 
-#serial_1 = serial.Serial(port_one)
-print convert_one
-print convert_two
+#print 'Streaming from ', convert_one
+print 'Streaming from' , convert_two
 
-send_ser = serial.Serial(convert_two)
-
-read_ser = serial.Serial(convert_two, 19200, timeout=1)
+#read_ser = serial.Serial(convert_one, 230400, timeout=1)
+read_ser_n = serial.Serial(convert_two)
 
 while 1:
 
-	send_ser.write("streaming...")
-	x = read_ser.read(14)
-	print x
+	x = read_ser_n.read()
+	he = hex(ord((x)))
+	print he       
