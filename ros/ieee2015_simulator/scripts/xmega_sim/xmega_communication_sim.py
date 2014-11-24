2268fab3e517593528b8126dbcbb8b732690d377
zachgoins <zach.a.goins@gmail.com>
2014-11-24 10:43:09 -0500

    xMega - xMega Terminal sim w/ beginnings of type formatting

--- ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
+++ ros/ieee2015_simulator/scripts/xmega_sim/xmega_communication_sim.py
@@ -8,7 +8,6 @@ import subprocess
 import sys
 import serial
 import time
-import os
 
 print 
 print "Xmega simulation setup..."
@@ -36,8 +35,59 @@ print 'Waiting for signal from xmega driver at ' , convert_two
 #read_ser = serial.Serial(convert_one, 230400, timeout=1)
 read_ser_n = serial.Serial(convert_two)
 
+first_types = ['0x5e','0xC0','0x43','0x80']
+second_types = ['0xC0','0xC0']
+third_types = ['0x01','0x02','0x03','0x04']
+
+Messgage = []
+
 while 1:
 
-	x = read_ser_n.read()
-	he = hex(ord((x)))
-	print he       
+	raw_input = read_ser_n.read()
+	convert_to_hex = hex(ord((raw_input))) # Need to set values to fill Message array and iterate through it
+
+# ------------------------------------------------------------------------------
+
+	for x in range(0, len(first_types)):
+		if convert_to_hex == first_types[x]:
+			print convert_to_hex # -------------- Testing print statement
+			boo = True
+		else:
+			boo = False
+
+	if boo == False:
+		print "Message Type Error"
+
+# ------------------------------------------------------------------------------
+
+	boo = True
+	for x in range(0, len(second_types)):
+		if convert_to_hex == second_types[x]:
+			print convert_to_hex # -------------- Testing print statement
+			boo = True
+		else: 
+			boo = False
+
+	if boo == False:
+		print "Second Type Error"
+
+# ------------------------------------------------------------------------------
+
+	boo = True
+	for x in range(0, len(third_types)):
+		if convert_to_hex == third_types[x]:
+			print convert_to_hex # -------------- Testing print statement
+			boo = True
+		else:
+			 boo = False
+
+	if boo == False:
+		print "Third Type Error"
+
+# ------------------------------------------------------------------------------
+
+
+		
+	
+
+
