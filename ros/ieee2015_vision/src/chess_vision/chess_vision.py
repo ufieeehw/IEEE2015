f03cde5c42383a6c62761627b6787aad53482a59
Pocathoughts <christinemoore321@gmail.com>
2014-11-30 00:27:53 -0500

    CHESS: Condensed the FE method, will PEP8 it tomorrow

--- ros/ieee2015_vision/src/chess_vision/chess_vision.py
+++ ros/ieee2015_vision/src/chess_vision/chess_vision.py
@@ -11,7 +11,8 @@ import FindPiece
 
 #Vision just needs to return a new_occupancy_grid and the coordinates
 #the rest will be taken care of in mission planner based off of the current architecture
-
+global x
+global y
 
 def get_Occupancy_and_Coordinates(img):
 		########################################################
@@ -23,7 +24,7 @@ def get_Occupancy_and_Coordinates(img):
 		#img = cv2.imread(filename)
 		#gray = cv2.imread(filename, 0)
 		########################################################
-
+		img = cv2.resize(img, (640, 480)) 
 		#Get the angle of rotation needed and rotate
 		#rotated image is really the thing we need because it will be the best final image to use
 		theta = baseLine.findAngle(img)
@@ -40,7 +41,7 @@ def get_Occupancy_and_Coordinates(img):
 		
 		#believe this step is no longer necessary because of the architecture
 		#Load squares with their starting state
-		#Squares.loadPieces(squares, pieceTypeArray)
+		#Squares.loadPieces(squares, board_state)
 
 		squaresCropped = Squares.getSquaresCropped(squares, rotatedImage)
 
@@ -51,165 +52,96 @@ def get_Occupancy_and_Coordinates(img):
 
 
 def giveForsythEdwardsNotation(board_state, w_or_b_turn):
-		###############I know 100% there is a way to consolidate this but for right now this is what I'm going to do###################3
-		###Initialize row stuff for final concatenated string
-		row1 = []
-		row2 = []
-		row3 = []
-		row4 = []
-		row5 = []
-		row6 = []
-		row7 = []
-		row8 = []
+	x = 0
+	y = 8
+	rows = []
 
-		string1 = ""
-		string2 = ""
-		string3 = ""
-		string4 = ""
-		string5 = ""
-		string6 = ""
-		string7 = ""
-		string8 = ""
+	for i in range(0, 8):
+		rows.append(board_state[x:y])
+		x = x+8
+		y = y +8
+	# items start through end-1
+		
 
-		counter1 = 0
-		counter2 = 0
-		counter3 = 0
-		counter4 = 0
-		counter5 = 0
-		counter6 = 0
-		counter7 = 0
-		counter8 = 0
 
-		for i in range(0, 8):
-		    row1.append(pieceTypeArray[i])
+	
+##############converting e to integer needed#####################3
 
-		for i in range(8, 16):
-		    row2.append(pieceTypeArray[i])
+	counter = 0
 
-		for i in range(16, 24):
-		    row3.append(pieceTypeArray[i])
+	for j in range(0, 8):
+		if (rows[j] == 'e'):
+			counter = counter + 1
+			rows[j] = ' '
+			if(j == 7):
+				rows[j] = str(counter)
+		else: 
+			if(counter != 0):
+				rows[j-1] = str(counter)
+			counter = 0
 
-		for i in range(24, 32):
-		    row4.append(pieceTypeArray[i])
 
-		for i in range(32, 40):
-		    row5.append(pieceTypeArray[i])
+	
+	string = []
 
-		for i in range(40, 48):
-		    row6.append(pieceTypeArray[i])
+	for k in range(0, 8):
+		string.append("".join(rows[k]))
+		
 
-		for i in range(48, 56):
-		    row7.append(pieceTypeArray[i])
+	
+	
+#string1 = ''.join(row1)
 
-		for i in range(56, 64):
-		    row8.append(pieceTypeArray[i])
+	
 
-		##############converting e to integer needed#####################3
+	backslash = "/"
+	combined__board_string = string[0] + backslash + string[1] + backslash + string[2] + backslash + string[3] + backslash + string[4] + backslash + string[5] + backslash + string[6] + backslash + string[7]
 
-		for i in range(0, 8):
-		    if(row1[i] == 'e'):
-		        counter1 = counter1 + 1
-		        row1[i] = ''
-		        if(i == 7):
-		            row1[i] = str(counter1)
-		    else:
-		        if(counter1 != 0):
-		            row1[i - 1] = str(counter1)
-		        counter1 = 0
+	combined__board_string
+	finalFE = combined__board_string + " " + w_or_b_turn
+	print finalFE
+	return finalFE
 
-		for i in range(0, 8):
-		    if(row2[i] == 'e'):
-		        counter2 = counter2 + 1
-		        row2[i] = ''
-		        if(i == 7):
-		            row2[i] = str(counter2)
-		    else:
-		        if(counter2 != 0):
-		            row2[i - 1] = str(counter2)
-		        counter2 = 0
-
-		for i in range(0, 8):
-		    if(row3[i] == 'e'):
-		        counter3 = counter3 + 1
-		        row3[i] = ''
-		        if(i == 7):
-		            row3[i] = str(counter3)
-		    else:
-		        if(counter3 != 0):
-		            row3[i - 1] = str(counter3)
-		        counter3 = 0
-
-		for i in range(0, 8):
-		    if(row4[i] == 'e'):
-		        counter4 = counter4 + 1
-		        row4[i] = ''
-		        if(i == 7):
-		            row4[i] = str(counter4)
-		    else:
-		        if(counter4 != 0):
-		            row4[i - 1] = str(counter4)
-		        counter4 = 0
-
-		for i in range(0, 8):
-		    if(row5[i] == 'e'):
-		        counter5 = counter5 + 1
-		        row5[i] = ''
-		        if(i == 7):
-		            row5[i] = str(counter5)
-		    else:
-		        if(counter5 != 0):
-		            row5[i - 1] = str(counter5)
-		        counter5 = 0
-
-		for i in range(0, 8):
-		    if(row6[i] == 'e'):
-		        counter6 = counter6 + 1
-		        row6[i] = ''
-		        if(i == 7):
-		            row6[i] = str(counter6)
-		    else:
-		        if(counter6 != 0):
-		            row6[i - 1] = str(counter6)
-		        counter6 = 0
-
-		for i in range(0, 8):
-		    if(row7[i] == 'e'):
-		        counter7 = counter7 + 1
-		        row7[i] = ''
-		        if(i == 7):
-		            row7[i] = str(counter7)
-		    else:
-		        if(counter7 != 0):
-		            row7[i - 1] = str(counter7)
-		        counter7 = 0
-
-		for i in range(0, 8):
-		    if(row8[i] == 'e'):
-		        counter8 = counter8 + 1
-		        row8[i] = ''
-		        if(i == 7):
-		            row8[i] = str(counter8)
-		    else:
-		        if(counter8 != 0):
-		            row8[i - 1] = str(counter8)
-		        counter8 = 0
-
-		string1 = ''.join(row1)
-		string2 = ''.join(row2)
-		string3 = ''.join(row3)
-		string4 = ''.join(row4)
-		string5 = ''.join(row5)
-		string6 = ''.join(row6)
-		string7 = ''.join(row7)
-		string8 = ''.join(row8)
-
-		print string5
-
-		backslash = "/"
-		combined__board_string = string1 + backslash + string2 + backslash + string3 + backslash + string4 + backslash + string5 + backslash + string6 + backslash + string7 + backslash + string8
-
-		finalFE = combined__board_string + " " + w_or_b_turn
-		print finalFE
-		return finalFE
 
 		##########################################################End of creating board part of FE Notation###################################################
+
+def findDifferencesArrays(old_occupancy_grid, new_occupancy_grid, board_state):
+	missingPiece = ''
+	indexOfFound = -1
+
+
+	for i in range(0, 64):
+		#this first if statement should be called no matter what 
+		#for a piece to move or to make a kill
+		#the piece must orignally be in a square and then 
+		#no longer be in that same square
+		if ((old_occupancy_grid[i] == 1 or old_occupancy_grid[i] == 2) and new_occupancy_grid[i] == 0):
+			#this call tells us if a piece is no longer where it once was
+			#hence we have found the "missing piece" 
+			missingPiece = board_state[i]
+			#then we need to set the board_state back to empty since 
+			#we have already identified it as empty
+			board_state[i] ='e'
+
+		#this elif is the case in which a square becomes occupied
+		elif (old_occupancy_grid[i] == 0 and (new_occupancy_grid[i] == 1 or new_occupancy_grid[i] == 2)):
+			#we know that we have found our missing piece
+			#we need to eventually replace our board_state
+			#with our missing piece so we need the index of where it moved
+			indexOfFound = i
+
+		#FOR THE NEXT TWO ELIFS, THESE ARE THE CASES IN WHICH A PIECE IS KILLED
+		#TO IDENTIFY OUR FOUND PIECE WE SPECIFICALLY NEED TO KNOW WHAT COLOR
+
+		#this elif statement is saying that a blue piece killed an orange piece
+		#we have found a change, hence where our missing piece moved to 
+		elif (old_occupancy_grid[i] == 1 and new_occupancy_grid[i] == 2): 
+			indexOfFound = i 
+
+		#this elif is if an orange killed a blue piece
+		elif (old_occupancy_grid[i] == 2 and new_occupancy_grid[i] == 1):
+			indexOfFound = i
+			
+		board_state[indexOfFound] = missingPiece
+
+	return board_state
