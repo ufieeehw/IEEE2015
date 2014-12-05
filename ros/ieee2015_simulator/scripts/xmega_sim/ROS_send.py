bc357ef5579c7c197f0df020408f06833ac55d1d
zachgoins <zach.a.goins@gmail.com>
2014-12-05 14:32:19 -0500

    Xmega Sim - Impliment ROS side simulation

--- ros/ieee2015_simulator/scripts/xmega_sim/ROS_send.py
+++ ros/ieee2015_simulator/scripts/xmega_sim/ROS_send.py
@@ -4,11 +4,6 @@ import rospy
 import os
 import argparse
 
-
-import roslib; roslib.load_manifest('ieee2015_xmega_driver')
-import xmega_driver
-from ieee2015_xmega_driver.msg import XMega_Message
-
 #import all messages used to pass data through xMega Driver
 
 from std_msgs.msg import Header, Float64, String
@@ -16,19 +11,50 @@ from geometry_msgs.msg import Point, PointStamped, PoseStamped, Pose, Quaternion
 from sensor_msgs.msg import Imu
 from ieee2015_xmega_driver.msg import XMega_Message
 
-def talk(self):
 
-	rospy.init_node('xmega_codes', anonymous=True)
-	pub = rospy.Publisher('robot/debug', String, queue_size=10)
-	r = rospy.Rate(2) 
+rospy.init_node('xmega_codes', anonymous=True)
 
-	while not rospy.is_shutdown():
-		self.serial_proxy.add_message('ros_debug', "Test")
+def debug_poll():
+
+	pub = rospy.Publisher('robot/debug', String, queue_size=1)
+	r = rospy.Rate(5) 
+
+	count = 0
+	while not rospy.is_shutdown() and count == 0:
+		string = "debug"
 		rospy.loginfo(string)
 		pub.publish(string)
 		r.sleep()
+		count+=1
 
-if __name__ == '__main__':
-	try:
-		talk('ros_debug')
-	except rospy.ROSInterruptException: pass
+def imu_poll():
+
+	pub = rospy.Publisher('robot/imu', String, queue_size=1)
+	r = rospy.Rate(5) 
+
+	count = 0
+	while not rospy.is_shutdown() and count == 0:
+		string = "imu"
+		rospy.loginfo(string)
+		pub.publish(string)
+		r.sleep()
+		count+=1
+
+'''def step_motor_send():
+
+	pub = rospy.Publisher('robot/desired_velocity', String, queue_size=1)
+	r = rospy.Rate(5) 
+
+	count = 0
+	while not rospy.is_shutdown() and count == 0:
+		string = "stepper motor"
+		rospy.loginfo(string)
+		pub.publish(string)
+		r.sleep()
+		count+=1
+		'''
+
+while True:
+	debug_poll()
+	imu_poll()
+	#step_motor_send()
