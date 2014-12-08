bd0f40065e4787d3f85a5067560e075f36ac9e79
Austin Baylis <austinbaylis@gmail.com>
2014-12-08 18:24:58 -0500

    pid.c: started reimplementation.

--- xmega/pid.c
+++ xmega/pid.c
@@ -1,17 +1,17 @@
-#include "pid.h"
-#include "pololu.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "pid.h"
+#include "pololu.h"
+
 #define constP 120
 #define constI 0
 #define constD 0.0004
 
 //no
-
 #define TEST_CPU 32000000
 
 //Testing shit
@@ -26,10 +26,26 @@ static PORT_t *wheelPort2 = &PORTB; // Originally PORTD
 static float radPerTick = (2*M_PI)/1856.0;
 static const float sampleTime = 10e-3;
 
-static pololu_t pololu_1;
-static pololu_t pololu_2;
-static pololu_t pololu_3;
-static pololu_t pololu_4;
+static pololu_t pololu_1 = {
+	.PORT = &PORTD;
+	.TC2 = &TCD2;
+	.motor2 = false;
+};
+static pololu_t pololu_2 = {
+	.PORT = &PORTD;
+	.TC2 = &TCD2;
+	.motor2 = true;
+};
+static pololu_t pololu_3 = {
+	.PORT = &PORTF;
+	.TC2 = &TCF2;
+	.motor2 = false;
+};
+static pololu_t pololu_4 = {
+	.PORT = &PORTF;
+	.TC2 = &TCF2;
+	.motor2 = true;
+};
 
 static Encoder_History leftfront_history; 
 static Encoder_History leftrear_history;
@@ -50,55 +66,36 @@ typedef struct{
 } pid_wheel_data_t;
 
 static const pid_wheel_data_t DEFAULT = {
-	.AVG_speed = 0, .ticks = 0, .input = 0, .output = 0, .setpoint = 0, .errSum = 0, .lastErr = 0, .kp = 0, .ki = 0, .kd = 0, .pid_last_ticks =0, .odometry_last_ticks = 0
+	.AVG_speed = 0,
+	.ticks = 0,
+	.input = 0,
+	.output = 0,
+	.setpoint = 0,
+	.errSum = 0,
+	.lastErr = 0,
+	.kp = 0,
+	.ki = 0,
+	.kd = 0,
+	.pid_last_ticks =0,
+	.odometry_last_ticks = 0
 };
 
+// Array of wheel data (per wheel?)
 static pid_wheel_data_t wheelData[4];
 
-void usart_putc(char c)
-{
-	while (!(USARTC0.STATUS & USART_DREIF_bm));
-	USARTC0.DATA = c;
-}
-
-void usart_sendstring(char *s)
-{
-	while (*s) {
-		usart_putc(*s++);
-	}
-}
-
 void pid_init() {
-	
-  
-  
-	//wheelPort1 = PORTA;
-	//wheelPort2 = PORTB;
-	
 	//Initialize Pololus
-	pololu_1.PORT = &PORTD;
-	pololu_1.TC2 = &TCD2;
-	pololu_1.motor2 = false;
 	pololuInit(&pololu_1);
-	pololu_2.PORT = &PORTD;
-	pololu_2.TC2 = &TCD2;
-	pololu_2.motor2 = true;
 	pololuInit(&pololu_2);
-	pololu_3.PORT = &PORTF;
-	pololu_3.TC2 = &TCF2;
-	pololu_3.motor2 = false;	
 	pololuInit(&pololu_3);
-	pololu_4.PORT = &PORTF;
-	pololu_4.TC2 = &TCF2;
-	pololu_4.motor2 = true;
 	pololuInit(&pololu_4);
-	
+
 	//pololu_set_velocity(&pololu_4, 256);
-	
+
 	//Initialize wheelData to default values
 	for(int8_t x = 0; x < 4; x++)
 		wheelData[x] = DEFAULT;
-	
+
 	// Initialize motor frequency measurement timers.
 	wheelPort1->DIRCLR = 0x0F;
 	wheelPort1->PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
@@ -106,30 +103,33 @@ void pid_init() {
 	wheelPort1->INTCTRL = PORT_INT0LVL_LO_gc | PORT_INT1LVL_LO_gc;
 	wheelPort1->PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
 	wheelPort1->INT1MASK = 0x0A;
-	
+
 	wheelPort2->DIRCLR = 0x0F;
 	wheelPort2->PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
 	wheelPort2->INT0MASK = 0x05;
 	wheelPort2->INTCTRL = PORT_INT0LVL_LO_gc | PORT_INT1LVL_LO_gc;
 	wheelPort2->PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
 	wheelPort2->INT1MASK = 0x0A;
+
 	// Initialize the PID tick timer
 	pid_tick_timer->CTRLA = TC_CLKSEL_DIV64_gc; //Uses timer on portC since all timers on PORT E and D will be used for wheels.
 	pid_tick_timer->CTRLB = 0x00;
 	pid_tick_timer->CTRLC = 0x00;
 	pid_tick_timer->CTRLD = 0x00;
 	pid_tick_timer->CTRLE = 0x00;
-  
+
 	//pid_tick_timer->PER = round(F_CPU * sampleTime / 64.);
 	pid_tick_timer->PER = round(TEST_CPU * sampleTime / 64.);
 	pid_tick_timer->INTCTRLA = TC_OVFINTLVL_LO_gc;
+
 	pid_setTunings(constP, constI, constD, WHEEL1);
 	pid_setTunings(constP, constI, constD, WHEEL2);
 	pid_setTunings(constP, constI, constD, WHEEL3);
 	pid_setTunings(constP, constI, constD, WHEEL4);
-	
+
 	sei();
 }
+
 static void pid_compute(wheelNum num) {
 	pid_wheel_data_t *data = &wheelData[num];
 	// Compute all working error variables
@@ -154,12 +154,14 @@ static void pid_compute(wheelNum num) {
 	//Remember some things for later
 	data->lastErr = error;
 }
+
 void pid_setTunings(float Kp, float Ki, float Kd, wheelNum num) {
 	pid_wheel_data_t *data = &wheelData[num];
 	data->kp = Kp;
 	data->ki = Ki*sampleTime;
 	data->kd = Kd/sampleTime;
 }
+
 static void pid_measureSpeed(wheelNum num) {
 	pid_wheel_data_t *data = &wheelData[num];
 	cli();
@@ -174,10 +176,12 @@ static void pid_measureSpeed(wheelNum num) {
 	}
 	data->pid_last_ticks = ticks;
 }
+
 float pid_getSpeed(wheelNum num) {
 	pid_wheel_data_t *data = &wheelData[num];
 	return data->AVG_speed;
 }
+
 void pid_setSpeed(float speed, wheelNum num) {
 	pid_wheel_data_t *data = &wheelData[num];
 	data->setpoint = speed;
@@ -196,6 +200,7 @@ void pid_set_speed_handler(float speed) {
 	for(int i = 0; i < 4; i++) pid_setSpeed(speed, (wheelNum)i);
 	sei();
 }
+
 /*
 void pid_get_speed_handler(char* message, uint8_t len) {
 	char wheelSpeeds[16];
@@ -205,6 +210,7 @@ void pid_get_speed_handler(char* message, uint8_t len) {
 	uart_float_to_char32((char*)&wheelSpeeds[12], pid_getSpeed(WHEEL4));
 	uart_send_msg_block(PIDgetSpeed, wheelSpeeds, 17);
 }*/
+
 static void pid_get_odometry(float* returnData) {
 	for(int i = 0; i < 4; i++) {
 		pid_wheel_data_t *data = &wheelData[i];
@@ -217,6 +223,7 @@ static void pid_get_odometry(float* returnData) {
 		data->odometry_last_ticks = ticks;
 	}
 }
+
 /*
 void pid_get_odometry_handler(char* message, uint8_t len) {
 	char odometry[16];
@@ -228,12 +235,15 @@ void pid_get_odometry_handler(char* message, uint8_t len) {
 	uart_send_msg_block(PIDgetOdometry, odometry, 17);
 }
 */
+
 static inline void pid_set_speed_multiplier(float val) {
 	radPerTick = val;
 }
+
 static inline float pid_get_speed_multiplier(void) {
 	return radPerTick;
 }
+
 // Because the multiplier is a floating point value, we'll multiply it by 1000 first, and then send it.
 // Or, if we're recieving it, we'll divide it by 1000.
 /*
@@ -243,11 +253,13 @@ void pid_get_speed_multiplier_handler(char* message, uint8_t len) {
 	uart_send_msg_block(PIDgetMultiplier, multiplier, 3);
 }
 */
+
 /*
 void pid_set_speed_multiplier_handler(char* message, uint8_t len) {
 	pid_set_speed_multiplier(uart_int16_to_float(message));
 }
 */
+
 ISR(PID_TICK_OVF) {
 	pid_measureSpeed(WHEEL1);
 	pid_measureSpeed(WHEEL2);
@@ -263,6 +275,7 @@ ISR(PID_TICK_OVF) {
 	pololu_set_velocity(&pololu_3, wheelData[WHEEL3].output);
 	pololu_set_velocity(&pololu_4, wheelData[WHEEL4].output);
 }
+
 unsigned int grayToBinary(unsigned int num)
 {
 	unsigned int mask;
@@ -333,6 +346,7 @@ ISR(PORTE_INT0_vect){
 	wheelData[WHEEL1].ticks += difference;
 	old_value = new_value;
 }
+
 ISR(PORTE_INT1_vect){
 	// pins 1 and 3
 	static int8_t old_value = 0;
@@ -343,6 +357,7 @@ ISR(PORTE_INT1_vect){
 	wheelData[WHEEL2].ticks += difference;
 	old_value = new_value;
 }
+
 ISR(PORTB_INT0_vect){
 	// pins 0 and 2
 	static int8_t old_value = 0;
@@ -353,6 +368,7 @@ ISR(PORTB_INT0_vect){
 	wheelData[WHEEL3].ticks += difference;
 	old_value = new_value;
 }
+
 ISR(PORTB_INT1_vect){
 	// pins 1 and 3
 	static int8_t old_value = 0;
