59e9cfdf30c7d935159842a9764fb69eccaa2fae
zachgoins <zach.a.goins@gmail.com>
2014-12-11 10:17:25 -0500

    xmega - Run for full xmega_driver simulation

--- ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
+++ ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
 echo 
 echo
 echo "BEGIN FULL SIMULATION SETUP"
 echo "<-------------------------------------------------------------->"
-echo "Opened XMEGA SIMULATOR -->"
-echo "Opened ROS SIMULATOR -->"
-echo "Please begin xMega and Port Setup by entering password in new window"
-echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete"
-echo 
-
 gnome-terminal --title="XMEGA SIMULATOR" -e 'bash -c "sudo python xmega_communication_sim.py"' &
+echo "Opened XMEGA SIMULATOR -->"
 gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "python ROS_send.py"' &
-roslaunch ieee2015_xmega_driver xmega_driver.launch
+echo "Opened ROS SIMULATOR -->"
+echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete"
+gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"' &
+echo "Opened ROSLAUNCH - XMEGA_DRIVER -->"
+echo 
+echo "NOW RUNNING FULL SIMULATION"
+echo 
