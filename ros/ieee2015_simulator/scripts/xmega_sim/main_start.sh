70ebb89033dc9429d41d2211bc2961add404046c
zachgoins <zach.a.goins@gmail.com>
2014-12-10 23:49:31 -0500

    Xmega - Implement master start script

--- ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
+++ ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
@@ -10,17 +10,6 @@ echo "Please begin xMega and Port Setup by entering password in new window"
 echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete"
 echo 
 
-echo "Enter [y/yes] when ready to continue setup and launch Xmega Driver"
-echo 
-
-gnome-terminal --title="XMEGA SIMULATOR" -e 'bash -c "sudo python xmega_communication_sim.py;exec $SHELL"' &
-gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "python ROS_send.py;exec $SHELL"' &
-
-read cont
-
-if [[ ($cont == 'y' ) || ( $cont == 'yes' ) ]]; then
-	gnome-terminal --title="XMEGA DRIVER" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"' 
-	echo 
-	echo "GO GO GO"
-	echo
-fi
+gnome-terminal --title="XMEGA SIMULATOR" -e 'bash -c "sudo python xmega_communication_sim.py"' &
+gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "python ROS_send.py"' &
+gnome-terminal --title="XMEGA DRIVER" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"' 
