71a4e0da9e50b10119b863a55c318e21db343110
zachgoins <zach.a.goins@gmail.com>
2014-12-15 23:39:03 -0500

    xmega - Added type parsing and user input

--- ros/ieee2015_simulator/scripts/xmega_sim/ROS_send.py
+++ ros/ieee2015_simulator/scripts/xmega_sim/ROS_send.py
@@ -1,10 +1,10 @@
 #/usr/bin/env python   
 
 import os
-import argparse
 import rospy
 import time
 import codecs
+import sys
 
 #import all messages used to pass data through xMega Driver
 
@@ -13,16 +13,60 @@ from geometry_msgs.msg import Point, PointStamped, PoseStamped, Pose, Quaternion
 from sensor_msgs.msg import Imu
 from ieee2015_xmega_driver.msg import XMega_Message
 
-# -------------------------- Function Definitions -----------------------------------------------
+# create paths to use for type parsing
+
+default_path_to_types = os.path.join('..', '..', '..', '..', 'xmega', 'types.h')
+default_path_to_parse= os.path.join('..', '..', '..', '..', 'ros', 'ieee2015_xmega_driver', 'src', 'xmega_driver')
+sys.path.insert(0, default_path_to_parse)
+from parse_types import parse_types_file
+
+global types_array # array to hold parsed types data structure
+global found_bool  # boolean to use in main loop for command compliance check
+
+
+# <<-------------------------------------- Function Definitions ------------------------------------------------>>
 
 def to_ascii(raw):
+
+	# Convert HEX value to ascii character
+
 	string = str(raw)
 	asc = string.encode(encoding='UTF-8',errors='strict')
 	return asc
 
+# ---------------------------------------------------------------------------------------------
 
-# -------------------------- Subscriber Definitons ----------------------------------
+def types_parse():
 
+	global types_array
+	parsed_types = parse_types_file(default_path_to_types)				
+
+	# size two dimensional array to be list length amount of rows with two collumns
+	# index 0 is type name
+	# index 1 is hex value
+
+	types_array = [[0 for x in range(2)] for x in range(len(parsed_types))] 	
+
+	for x in range(0, len(parsed_types)):			# as long as the dictionary has values									
+		to_dict = parsed_types[x]					# convert list line to dictionary
+		types_array[x][0] = to_dict['type_name']	# add types to array
+		types_array[x][1] = to_dict['hex_name']		# add correlating ascii character to array
+
+# ---------------------------------------------------------------------------------------------
+
+def possible_types():
+
+	global types_array
+
+	print "<--- These are the possible types to send --->"
+	print
+
+	# iterate through array to print possible type values to send
+
+	for x in range(0, len(types_array)):
+		print types_array[x][0]
+
+# <----------------------------------- Subscriber Definitons ---------------------------------------------->
 
 def imu_poll():
 
@@ -37,65 +81,85 @@ def imu_poll():
 			rospy.loginfo("imu polled %s", item)
 		r.sleep()
 
+# <----------------------------------- Publisher Definitons ---------------------------------------------->
 
-# -------------------------- Publisher Definitons ------------------------------------
+def debug_poll(index):
+	global types_array
 
-
-def debug_poll():
-
-	pub = rospy.Publisher('robot/debug', String, queue_size=10)
+	pub = rospy.Publisher('robot/debug', String, queue_size=1)
 	r = rospy.Rate(5) 
 
-	msg = [0x40,0x00]
+	msg = [types_array[index][1], 0]
 	count = 0
 
 	while not rospy.is_shutdown() and count != len(msg):
 
 		for item in msg:
-			converted = to_ascii(item)
-			pub.publish(converted)
+			pub.publish(item)
 			rospy.loginfo("Debug polled %s", item)
 			count = count + 1
 		r.sleep()
 
+# ---------------------------------------------------------------------------------------------
 
 def step_motor_send():
 
-	pub = rospy.Publisher('robot/desired_velocity', String, queue_size=1)
+	pub = rospy.Publisher('robot/desired_velocity', TwistStamped, queue_size=1)
 	r = rospy.Rate(5) 
 
-	msg = [0x80,0x00,0x01]
-	count = 0
+	msg = [0, 0]
 
-	while not rospy.is_shutdown() and count != len(msg):
-		for item in msg:
-			converted = to_ascii(item)
-			pub.publish(converted)
-			rospy.loginfo("Stepper Motor sent %s", item)
-			count = count + 1
+	while not rospy.is_shutdown():
+		pub.publish(msg)
+		rospy.loginfo("Stepper Motor sent %s", item)
 		r.sleep()
 
 
-# ------------------------------- End of Function Definitions -----------------------------------------
+# <<----------------------------------- End of Function Definitions ------------------------------------------>>
 
+print 
 print "<------------------------------------------------------------------->"
-print "ROS side simulation"
+print
+print "<--- ROS SIDE SIMULATION"
 print 
 
-print "st --> stepper motor send"
-print "db --> debug send"
+types_parse()
+possible_types()
 
 rospy.init_node('xmega_codes', anonymous=True)
 
-# ------------------------------------ Begin Main Loop ------------------------------------------------
+
+# ----------------- Begin Main Loop ---------------------
 
 while not rospy.is_shutdown():
 
+	print 
+	global types_array
+	global found_bool
+
+	found_bool = False	# reset value on every loop 
+	
 	send_type = raw_input("Command: ", )
-	if send_type == "st":
-		step_motor_send()
-	if send_type == "db":
-		debug_poll()
+
+	# loop through types array until a matching type is found
+	# if command matches type in array, coresponding function is called
+	# if no type with the same name is found, error is printed
+
+	for x in range(0, len(types_array)):
+		if types_array[x][0] == send_type:
+			found_bool = True
+			if send_type == "DEBUG_TYPE":
+				debug_poll(x)
+			if send_type == "STEP_MOTOR_TYPE":
+				step_motor_send()
+
+	if found_bool == False:
+		print
+		print "TYPE NOT FOUND"
+		print 
+		possible_types()
+		
+
 
 	
 
