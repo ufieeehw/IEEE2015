cdafc40f189434724c78f27749d844c6cc34e0e7
zachgoins <zach.a.goins@gmail.com>
2014-12-16 23:26:42 -0500

    xmega - impliment start script for full simulation

--- ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
+++ ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
@@ -5,7 +5,7 @@ echo "BEGIN FULL SIMULATION SETUP"
 echo "<-------------------------------------------------------------->"
 gnome-terminal --title="XMEGA SIMULATOR" -e 'bash -c "sudo python xmega_communication_sim.py"' &
 echo "Opened XMEGA SIMULATOR -->"
-gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "python ROS_send.py"' &
+gnome-terminal --title="ROS SIMULATOR" --geometry="70x30" -e 'bash -c "python ROS_send.py"' &
 echo "Opened ROS SIMULATOR -->"
 echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete"
 gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"' &
