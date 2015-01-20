839f760e92e5c93def641ced367525cf54a85584
zachgoins <zach.a.goins@gmail.com>
2015-01-20 15:56:27 -0500

    Script to activate xmega sim and test accuracy

--- ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
+++ ros/ieee2015_simulator/scripts/xmega_sim/main_start.sh
@@ -5,11 +5,12 @@ echo "BEGIN FULL SIMULATION SETUP"
 echo "<-------------------------------------------------------------->"
 gnome-terminal --title="XMEGA SIMULATOR" -e 'bash -c "sudo python xmega_communication_sim.py"' &
 echo "Opened XMEGA SIMULATOR -->"
+echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete" &
+gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"'
+echo "Opened ROSLAUNCH - XMEGA_DRIVER -->"
+sleep 3
 gnome-terminal --title="ROS SIMULATOR" --geometry="70x30" -e 'bash -c "python ROS_send.py"' &
 echo "Opened ROS SIMULATOR -->"
-echo "roslaunch will not begin until XMEGA SIMULATOR setup is complete"
-gnome-terminal --title="ROS SIMULATOR" -e 'bash -c "roslaunch ieee2015_xmega_driver xmega_driver.launch"' &
-echo "Opened ROSLAUNCH - XMEGA_DRIVER -->"
 echo 
 echo "NOW RUNNING FULL SIMULATION"
 echo 
---
+++ ros/ieee2015_simulator/scripts/xmega_sim/unit_test.py
@@ -0,0 +1,4 @@
+import unittest
+import ROS_send
+import xmega_communication_sim.py
+# Got a lot done today
