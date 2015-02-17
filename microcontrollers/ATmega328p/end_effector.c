f169c491f71a7cabe9db4e879bce6c783f378100
zachgoins <zach.a.goins@gmail.com>
2015-02-16 20:41:38 -0500

    Xmega and ATmega HW folders

---
+++ microcontrollers/ATmega328p/end_effector.c
@@ -0,0 +1,104 @@
+/*
+
+UART protocol for UF 2015 IEEE end effector
+Created on: 10-February-2015
+Created by: Zach Goins
+
+*/
+
+#include "end_effector.h"
+ 
+// Configures USART and Baud Rate
+void UART_init(void)
+{
+	// Not necessary; initialize anyway
+	DDRD |= (1<<1);
+	DDRD &= (0<<0);
+ 
+	// Set baud rate; lower byte and top nibble
+	UBRR0H = ((SCALER) & 0xF00);
+	UBRR0L = (uint8_t) ((SCALER) & 0xFF);
+ 
+	TX_START();
+	RX_START();
+	RX_INTEN();
+	TX_INTEN();
+ 
+	// Set frame format = 8-N-1
+	UCSR0C = (0x03 << UCSZ00);
+ 
+}
+ 
+//Returns a byte from the serial buffer
+uint8_t get_byte(void)
+{
+	// Check to see if something was received
+	while (!(UCSR0A & _BV(RXC0)));
+	return (uint8_t) UDR0;
+}
+ 
+// Transmits a byte
+void put_byte(unsigned char data)
+{
+	// Stay here until data buffer is empty
+	while (!(UCSR0A & _BV(UDRE0)));
+	UDR0 = (unsigned char) data;
+ 
+}
+
+// Receive multiple Bytes before taking action
+void get_string(int length)
+{
+	// length specifies length of incoming message
+	for (int x = 0; x < length; x++)
+	{
+		recv_bytes[x] = get_byte();
+	}	
+}
+
+// Depending on received bytes, activates solenoids 
+void solenoid(){
+	
+	if (recv_bytes[1] == SOL_COM_IN)
+	{
+		SOL_IN;
+		putByte(0x00);
+	}
+	else if (recv_bytes[1] == SOL_COM_OUT)
+	{
+		SOL_OUT;
+		putByte(0x01);
+	}
+	
+}
+ 
+int main(void)
+{ 
+	cli();				// disable interrupts
+	UART_init();	
+	sei();				// enable interrupts
+	SOL_INIT			// enable solenoid output 
+	
+    while(1);
+}
+
+ISR(USART_RX_vect)
+{	
+	get_string(2);
+	
+	if ((recv_bytes[0] == SOL)
+	{
+		solenoid();
+	}
+	if ((recv_bytes[0] == SERVO)
+	{
+		
+	}
+	
+	put_byte(TRANS_COMPLT);
+}
+
+ISR(USART_TX_vect)
+{
+	
+}
---
+++ microcontrollers/ATmega328p/end_effector.h
@@ -0,0 +1,40 @@
+/* 
+
+UART protocol for UF 2015 IEEE end effector
+Created on: 10-February-2015
+Created by: Zach Goins
+
+*/
+
+#define F_CPU 16000000UL
+
+#define DESIRED_BAUD	9600							// Baud rate (9600 is default)
+#define SCALER			(F_CPU/16)/(DESIRED_BAUD - 1)	// Used for UBRRL and UBRRH
+#define TX_START()		UCSR0B |= (1<<TXEN0)			// Enable TX
+#define RX_START()		UCSR0B |= (1<<RXEN0)			// Enable RX
+#define RX_INTEN()		UCSR0B |= (1<<RXCIE0)			// Enable interrupt on RX complete
+#define TX_INTEN()		UCSR0B |= (1<<TXCIE0)			// Enable interrupt on TX complete
+
+#define SOL_INIT		DDRB = 0b00011000;				// Solenoid output to H-bridge on Pin 3 and 4
+#define SOL				0x6A							// This is a message about solenoids
+#define SOL_COM_IN		0x6B							// This message is telling us to pull solenoids in
+#define SOL_COM_OUT		0x6C							// This message is telling us to push solenoids out
+#define	SOL_IN			PORTB = 0b00001000;				// Pull solenoid in
+#define SOL_OUT			PORTB = 0b00010000;				// Release magnet and push out
+
+#define SERVO			0x7A							// This is a message about solenoids
+
+#define TRANS_COMPLT	0xFE							// Transmission complete
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+uint8_t recv_bytes[5];
+unsigned int servo_position;
+
+void UART_init(void);
+uint8_t get_byte(void);
+void put_byte(unsigned char data);
+void get_string(int length);
+void solenoid();
