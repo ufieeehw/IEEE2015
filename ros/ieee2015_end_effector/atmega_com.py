8708307aa5c2dec6a9a84520ff895e0c52b0538b
zachgoins <zach.a.goins@gmail.com>
2015-02-19 14:34:16 -0500

    Passes ROS messages to ATmega328P

--- ros/ieee2015_end_effector/atmega_com.py
+++ ros/ieee2015_end_effector/atmega_com.py
@@ -4,6 +4,8 @@ import serial
 import time
 import codecs
 
+#defines 
+
 
 # <--------------------------------Function Definitions--------------------------------->
 
@@ -23,13 +25,10 @@ def hex_determine(message):
 
 def read_from_serial(byte_num):
 
-    Message = serial.read(1)
-    
-    for x in range(0, byte_num):
+    Message = serial_port.read(byte_num)
 
-        temp = hex_determine(int.from_bytes(Message, byteorder='little'))
-        print (temp)
-        read_from_serial(byte_num - 1)
+    temp = hex_determine(int.from_bytes(Message, byteorder='little'))
+    print (temp)
 
 
 # <--------------------------------------------------------------------------------------->
@@ -38,21 +37,36 @@ def read_from_serial(byte_num):
 def available_ports():
     subprocess.call(["python", "-m", "serial.tools.list_ports"])
 
-
 # <--------------------------------------------------------------------------------------->
 
+def checksum(param1, param2):
+	#take two parameter
+	#subaract one from the other and take abs value
+	check = abs(ord(param1) -ord(param2))
+	return check
 
-def solenoid_message():
+def solenoid_message(in_or_out):
 
-    serial.write(str('j').encode())
-    time.sleep(.04)
-    serial.write(str('l').encode())
+    if in_or_out == 1:
+        check = checksum('j', 'k')
+        serial_port.write(str('j').encode())
+        serial_port.write(str('k').encode())
+       	serial_port.write(str(check).encode())
+
+
+    if in_or_out == 0:
+        check = checksum('j', 'l')
+        serial_port.write(str('j').encode())
+        serial_port.write(str('l').encode())
+       	serial_port.write(str(check).encode())
+
+    read_from_serial(2)	
 
 def servo_message():
 
-    serial.write(str('u').encode())
+    serial_port.write(str('u').encode())
     time.sleep(.04)
-    serial.write(str('i').encode())
+    serial_port.write(str('i').encode())
 
 
 # <--------------------------------------------------------------------------------------->
@@ -70,11 +84,21 @@ if sys.argv[1] == "-p":
     available_ports()
     sys.exit(0)
 
-serial = serial.Serial(sys.argv[1], baudrate=9600, timeout=0)
+serial_port = serial.Serial(sys.argv[1], baudrate=9600, timeout=0)
 
 # <-------------------------------------MAIN LOOP----------------------------------------->
 
 while 1:
 
-    solenoid_message()
-    read_from_serial(2)
+    solenoid_message(1)
+    solenoid_message(1)
+    solenoid_message(1)
+    solenoid_message(1)
+    solenoid_message(1)
+    time.sleep(1)
+    solenoid_message(0)
+    solenoid_message(0)
+    solenoid_message(0)
+    solenoid_message(0)
+    solenoid_message(0)
+    time.sleep(1)
