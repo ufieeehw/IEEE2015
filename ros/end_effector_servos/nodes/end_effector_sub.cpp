3f2ca3f2b635aeb9a2cc396555b9bb8dc3196cf4
Zach Goins <zach.a.goins@gmail.com>
2015-03-17 17:09:17 -0400

    End Effector: Added messages for all servos

--- ros/end_effector_servos/nodes/end_effector_sub.cpp
+++ ros/end_effector_servos/nodes/end_effector_sub.cpp
@@ -1,31 +1,3 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -41,122 +13,41 @@
 
 #include "high_level_commands.h"
 #include "communications.h"
-#include "end_effector_servos/Num.h"
+ #include "end_effector_servos/Num.h"
 
 
-void ListAllDynamixels(int usb2ax_index) {
-  int baud_num, dxl_id;
-  int model_num, firmware_version;
-  for (baud_num = 3; baud_num >= 0; baud_num--) {
-    if(InitDXL(usb2ax_index, baud_num) == 0 ) {
-      printf("Failed to open device: %d, baud num: %d\n", usb2ax_index, baud_num);
-      TerminateDXL();
-      return;
-    } else {
-      printf("Searching on device: %d, baud num: %d\n", usb2ax_index, baud_num);
-    }
-
-    for (dxl_id = 0; dxl_id < 200; dxl_id++) {
-      if (SendPing(dxl_id, &model_num, &firmware_version)) {
-        printf("Device found| Baud rate %d, ID: %d, Model number: %d, Firmware version: %d\n", baud_num, dxl_id, model_num, firmware_version);
-      }
-    }
-    }
-    // ID 200 is one of the controllers or something.
-    // ID 253 is USB2AX
-    // ID 254 is broadcast
-    for (dxl_id = 201; dxl_id < 253; dxl_id++) {
-      if (SendPing(dxl_id, &model_num, &firmware_version)) {
-        printf("Device found| Baud rate: %d, ID: %d, Model number: %d, Firmware version: %d\n", baud_num, dxl_id, model_num, firmware_version);
-      }
-    }
-  
-}
-
-void ListDevices() {
-  DIR * dir;
-  struct dirent * ent;
-  char* devices_dir = "/dev/";
-  char* expected_prefix = "ttyACM";
-  int i;
-  if ((dir = opendir (devices_dir)) != NULL) {
-    while ((ent = readdir (dir)) != NULL) {
-      if (strlen(expected_prefix) <= strlen(ent->d_name)) {
-        printf("%s\n", ent->d_name);
-      }
-      else{
-      }
-    }
-    closedir (dir);
-  } else {
-    printf("Couldn't find device directory: %s\n", devices_dir);
-  }
-}
-
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
 void chatterCallback(const end_effector_servos::Num::ConstPtr &num)
 {
-  SetPosition(5, num->position);
-  SetVelocity(5, num->velocity);
+  SetPosition(2, num->position_one);
+  SetVelocity(3, num->velocity_one);
+  SetLED(2,num->color_one);
+  SetPosition(3, num->position_two);
+  SetVelocity(3, num->velocity_two);
+  SetLED(3,num->color_two);
+  SetPosition(4, num->position_three);
+  SetVelocity(4, num->velocity_three);
+  SetLED(4,num->color_three);
 }
-// %EndTag(CALLBACK)%
+
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+   
   ros::init(argc, argv, "listener");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
   ros::Subscriber sub = n.subscribe("end_effector_servos", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
+  int device_num = 0; // Default to device ID
+  InitDXL(2,3);
+  InitDXL(3,3);
+  InitDXL(4,3);
+  SetLED(2,7);
+  SetLED(3,7);
+  SetLED(4,7);
 
-  int device_num = 1; // Default to device ID 0
-  InitDXL(5, 3);
-
-// %Tag(SPIN)%
   ros::spin();
-// %EndTag(SPIN)%
 
   return 0;
 }
-// %EndTag(FULLTEXT)%
