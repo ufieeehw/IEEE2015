0b1e4546ff7eb0d7938bb92fd58a11ca838f95c7
Christine Moore <christinemoore321@gmail.com>
2015-03-21 19:25:15 -0400

    ObjDetect: updating etchasketch

--- ros/ieee2015_vision/src/object_detection/etchasketch.py
+++ ros/ieee2015_vision/src/object_detection/etchasketch.py
@@ -3,78 +3,109 @@ import numpy as np
 import cv2.cv as cv
 
 def etchaSketch(img):
-	grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	hsv_etchasketch = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-	hsv_etchasketch = cv2.resize(hsv_etchasketch, (300,250))
+    hsv_etchasketch = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #hsv_etchasketch = cv2.resize(hsv_etchasketch, (300,250))
 
+    lower_white = np.array([70, 0, 170], np.uint8)  #estimated values until testing
+    upper_white = np.array([179, 20, 255], np.uint8) #estimated values until testing
+    # Searching the HSV image to extract the white colors
 
-	lower_white = np.array([70, 0, 170], np.uint8)  #estimated values until testing
-	upper_white = np.array([179, 20, 255], np.uint8) #estimated values until testing
-	# Searching the HSV image to extract the white colors
+    binary_img = cv2.inRange(hsv_etchasketch, lower_white, upper_white)
 
-	binary_img = cv2.inRange(hsv_etchasketch, lower_white, upper_white)
-	small_bin = cv2.resize(binary_img, (300, 250))
-
-
-
-	grayscale = cv2.resize(grayscale, (300, 250))
-
-	ret,thresh = cv2.threshold(grayscale,175,255,0)
-
-	cv2.imshow('binary', thresh)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows
+    kernel = np.ones((3, 3))
+    #kernel for eroding
+    kernel2 = np.ones((8,8), np.uint8)
+    #kernel for dilating
+    kernel3 = np.ones((12, 12), np.uint8)
+    eroded = cv2.erode(binary_img, kernel2)
+    dilated = cv2.dilate(eroded, kernel3)
+    closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel, iterations=10)
 
 
-	contours_k, hierarchy_k = cv2.findContours(thresh, 1, 2)
+    cv2.imshow('inrange small bin', closing)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+
+    ###FOLLOWING IS FOR GRAYSCALE ATTEMPT###
+    #seems to be extremely less effective, just grouping old code together
+    #grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #equ = cv2.equalizeHist(grayscale)
+    #grayscale = cv2.resize(equ, (300, 250))
+    #ret,thresh = cv2.threshold(grayscale,175,255,0)
+    #cv2.imshow('binary', equ)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows
+    #cv2.imshow('binary', thresh)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows
+    #contours_k, hierarchy_k = cv2.findContours(thresh, 1, 2)
+
+    #use distance as filter 
+    #user area
+    contours_knobs, hierarchy_knobs = cv2.findContours(binary_img, 1, 2)
+
+    cnt = contours_knobs[6] #Detected contours. Each contour is stored as a vector of points.
+    print cnt
+
+    
+
+    M = cv2.moments(cnt)
+    print M
+    
+    #area = cv2.contourArea(cnt)
+    cx_coord= int(M['m10']/M['m00'])
+    cy_coord = int(M['m01']/M['m00'])
+
+    print cx_coord
+    print cy_coord
+    print 'this is the len of contour knobs'
+    print len(contours_knobs)
 
 
-	contours_knobs, hierarchy_knobs = cv2.findContours(small_bin, 1, 2)
+    cv2.circle(img, (cx_coord, cy_coord), 50, (100, 255, 0), thickness=1, lineType=8, shift=0)
+    cv2.drawContours(img,[cnt],0,(255,0,0),-1)
+    
+  
+    ####START DISPLAY METHODS####
+    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
 
-	cnt = contours_k[0] #Detected contours. Each contour is stored as a vector of points.
-	M = cv2.moments(cnt)
-	    #area = cv2.contourArea(cnt)
-	cx_coord= int(M['m10']/M['m00'])
-	cy_coord = int(M['m01']/M['m00'])
+    equ = cv2.equalizeHist(grayscale)
+    #equ = cv2.resize(equ, (300,250))
+    cv2.imshow('detected circles', equ)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    circles = cv2.HoughCircles(equ,cv.CV_HOUGH_GRADIENT, 1.2, 100)
+          
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+        # draw the outer circle
+            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+    else:
+        print "You screwed up"
 
-	print cx_coord
-	print cy_coord
-
-	cv2.circle(grayscale, (cx_coord, cy_coord), 50, (100, 255, 0), thickness=1, lineType=8, shift=0)
-	cv2.drawContours(grayscale,[cnt],0,(255,0,0),-1)
-	      
-	return cx_coord, cy_coord
-
-	cv2.imshow('image w/ contours', grayscale)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-
-
-
-'''
-cv2.imshow("circles", circles)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #small = cv2.resize(image, (300, 250))
+    cv2.imshow('detected circles', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-if circles is not None:
-	circles = np.uint16(np.around(circles))
-	for i in circles[0,:]:
-	# draw the outer circle
-		cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
-		# draw the center of the circle
-		cv2.circle(image,(i[0],i[1]),2,(0,0,255),3)
-else:
-	print "You screwed up"
+    return cx_coord, cy_coord
 
-small = cv2.resize(image, (300, 250))
-cv2.imshow('detected circles', small)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('image w/ contours', grayscale)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-'''
+img = cv2.imread('Images/etch2.jpg')
+etchaSketch(img)
+
+
+
+
 
 
 
