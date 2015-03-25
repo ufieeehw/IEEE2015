f831244f1a7389f495c7f670aa49657c5d70ebe2
Christine Moore <christinemoore321@gmail.com>
2015-03-25 17:40:54 -0400

    ObjDetect: update to pep8

--- ros/ieee2015_vision/src/object_detection/ss_findColor.py
+++ ros/ieee2015_vision/src/object_detection/ss_findColor.py
@@ -1,16 +1,35 @@
-import cv2
-
-
-def findColor(brightX, brightY, blueX, blueY, greenX, greenY, minXPointL, maxXPointL, minYPointL, maxYPointL, minXPointR, maxXPointR, minYPointR, maxYPointR):
+def findColor(brightX, brightY, blueX, blueY, greenX, greenY, minXL, maxXL, minYL, maxYL, minXR, maxXR, minYR, maxYR):
     averageRow = (greenX + blueX)/2
 
+    print 'this is average row'
+    print averageRow
+
+    print 'this is bright center x'
+    print brightX
+    print 'this is bright center y'
+    print brightY
+
+    print 'this is greenY'
+    print greenY
+    print 'this is greenX'
+    print greenX
+
+    print 'this is blueY'
+    print blueY
+    print 'this is blueX'
+    print blueX
+
+    #  means that centerpoint of the bright spot is below the average row of green and blue
     if(brightX > averageRow and brightY > greenY and brightY < blueY):
-        return 1 #  we have a yellow button lighting up
+        return 1  # we have a yellow button lighting up
+    #  means that the centerpoint of the bright spot is above the average row of green and blue
     elif(brightX < averageRow and brightY > greenY and brightY < blueY):
-        return 3  #  we have a red button lighting up
-    elif(brightX < maxXPointL and brightX > minXPointL and brightY < maxYPointL and brightY > minYPointL):
-        return 2
-    elif(brightX < maxXPointR and brightX > minXPointR and brightY < maxYPointR and brightY > minYPointR):
-        return 4
+        return 3  # we have a red button lighting up
+    #  means that the centerpoint of the bright spot is between the boundaries of the green button
+    elif(brightX < maxXL and brightX > minXL and brightY < maxYL and brightY > minYL):
+        return 2  # we have a green button
+    #  mean the centerpoint of the bright spot is between the boundaries of the blue button
+    elif(brightX < maxXR and brightX > minXR and brightY < maxYR and brightY > minYR):
+        return 4  # we have a blue button
     else:
         return -1
