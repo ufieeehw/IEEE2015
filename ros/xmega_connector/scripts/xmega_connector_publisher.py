7d04049487f2c40534b27ff61d37388b6c2f2403
Jacob Panikulam <jpanikulam@ufl.edu>
2015-03-29 19:05:11 -0400

    XMega: Add new udev rules

--- ros/xmega_connector/scripts/xmega_connector_publisher.py
+++ ros/xmega_connector/scripts/xmega_connector_publisher.py
@@ -10,7 +10,7 @@ import math
 import numpy
 from std_msgs.msg import Header
 from xmega_connector.msg import XMEGAPacket
-from xmega_connector.srv import * #Echo, EchoRequest, EchoResponse
+from xmega_connector.srv import * # Echo, EchoRequest, EchoResponse
 from geometry_msgs.msg import TwistStamped, Twist, Vector3, PoseStamped, Pose, Point, Quaternion
 from tf import transformations
 
