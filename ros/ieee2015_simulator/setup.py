fc7fce6de64d2faeee1d3257f41217abc5d19c7d
Jacob Panikulam <jpanikulam@ufl.edu>
2015-03-29 19:00:13 -0400

    Simulator: Add multiline text box

--- ros/ieee2015_simulator/setup.py
+++ ros/ieee2015_simulator/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['view_sim'],
+    packages=['view_sim', 'sim_tools'],
     package_dir={'': 'src'},
 )
 
---
+++ ros/ieee2015_simulator/src/sim_tools/text_box.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Text_Box(object):
+    '''Text_Box()
+    '''
+    pygame.font.init()
+    font = pygame.font.SysFont("monospace", 15)
+
+
+    @classmethod
+    def draw(self, display, pos=(0, 0), color=(255, 255, 255), text="Empty!"):
+        ''' draw(display, pos=(0, 0), color=(255, 255, 255), text="Empty!"):
+
+        pos: In pygame coordinates
+        color: [0, 255]
+        text: Can by multiline, of arbitrary length
+
+        To change text during operation, use the "set_text method"
+        Ex:
+            >>> tb = Text_Box()
+            >>> tb.draw(display, text='hello')
+        or in a draw loop,
+            >>> tb.draw(display, pos, color, text)
+        '''
+        lines = text.splitlines()
+        width = height = 0
+        for l in lines:
+            width = max(width, self.font.size(l)[0])
+            height += self.font.get_linesize()
+
+        height = 0
+        for l in lines:
+            t = self.font.render(l, 0, color)
+            display.blit(
+                t, 
+                (pos[0], pos[1] + height)
+            )
+            height += self.font.get_linesize()
