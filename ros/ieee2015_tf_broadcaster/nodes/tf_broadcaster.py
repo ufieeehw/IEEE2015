a908684548eb05302a2838e57257020b27e3e088
LucasBA <lbassettaudain@ufl.edu>
2015-03-31 23:37:41 -0400

    TF: Corectly represent paralleogram with end of arm

--- ros/ieee2015_tf_broadcaster/nodes/tf_broadcaster.py
+++ ros/ieee2015_tf_broadcaster/nodes/tf_broadcaster.py
@@ -39,7 +39,7 @@ def read_base_servo(msg):
     global last_base_servo_position
     last_base_servo_position = msg.current_pos + base_angle_offset
     translation = (0.0, 0.0, 0.0) #assuming forward_camera is in the slot closest to edge
-    rotation = tf.transformations.quaternion_from_euler(0, np.pi / 2, last_base_servo_position) #rotates about the y axis to make the z axis along the motor, servo rotation value is sent to z rotation
+    rotation = tf.transformations.quaternion_from_euler(0, 0, last_base_servo_position) #rotates about the y axis to make the z axis along the motor, servo rotation value is sent to z rotation
     time = rospy.Time.now()
     
     tf_broad.sendTransform(translation, rotation, time, "/base_servo", "/robot")
@@ -48,7 +48,7 @@ def read_base_servo(msg):
 def read_shoulder_servo(msg):
     global last_shoulder_servo_position
     last_shoulder_servo_position = (msg.current_pos + shoulder_angle_offset)
-    translation = (-0.07358, 0, 0.02073) 
+    translation = (0.02073, 0, 0.07358) 
     rotation = tf.transformations.quaternion_from_euler(0.0, np.pi/2-last_shoulder_servo_position, 0.0) #may include a constant shift to follow axis reference conventions, will have a published angle from shoulder servo
     time = rospy.Time.now() 
 
@@ -59,19 +59,21 @@ def read_elbow_servo(msg):
     global last_elbow_servo_position
     global last_base_servo_position
     last_elbow_servo_position = -(msg.current_pos + elbow_angle_offset)
-    translation = (-0.148, 0, 0) #assume the servo is located at the elbow for simplicity  
-    rotation = tf.transformations.quaternion_from_euler(0, -last_elbow_servo_position + last_shoulder_servo_position, 0) #may include a constant shift to follow axis reference conventions, will have a published angle from elbow servo
+    translation = (0, 0, 0.148) #assume the servo is located at the elbow for simplicity  
+    rotation = tf.transformations.quaternion_from_euler(0, np.pi/2-last_elbow_servo_position + last_shoulder_servo_position, 0) #may include a constant shift to follow axis reference conventions, will have a published angle from elbow servo
     time = rospy.Time.now() 
 
     tf_broad.sendTransform(translation, rotation, time, "/elbow_servo", "/shoulder_servo")
 
-    send_wrist_joint()
+    send_wrist_joint(msg)
 
 
-def send_wrist_joint(): #end-effector always parallel to the floor, angle reationship between the wrist joint and elbow reference fram  
+def send_wrist_joint(msg): #end-effector always parallel to the floor, angle reationship between the wrist joint and elbow reference fram  
     '''Called in read_elbow_servo because it only depends on elbow'''
+    global last_elbow_servo_position
+    global last_base_servo_positiona
     translation = (-0.160, 0, 0) 
-    rotation = tf.transformations.quaternion_from_euler(0, 0, last_elbow_servo_position) #angle offset applied to published servo value
+    rotation = tf.transformations.quaternion_from_euler(0, -(-last_elbow_servo_position+last_shoulder_servo_position)+last_shoulder_servo_position-np.pi/2, 0) #angle offset applied to published servo value
     time = rospy.Time.now()
    
     tf_broad.sendTransform(translation, rotation, time, "/wrist_joint", "/elbow_servo")
@@ -80,8 +82,8 @@ def send_wrist_joint(): #end-effector always parallel to the floor, angle reatio
 def read_wrist_servo(msg):
     global last_wrist_servo_position
     last_wrist_servo_position = msg.current_pos
-    translation = (-0.03442, 0, 0) #constant offset 
-    rotation = tf.transformations.quaternion_from_euler(np.pi / 2, 0, last_wrist_servo_position) #published by end servo
+    translation = (0, 0, 0.03442) #constant offset 
+    rotation = tf.transformations.quaternion_from_euler(0, 0, last_wrist_servo_position) #published by end servo
     time = rospy.Time.now() 
 
     tf_broad.sendTransform(translation, rotation, time, "/wrist_servo", "/wrist_joint")
