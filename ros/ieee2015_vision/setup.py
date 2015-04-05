685f93cf04b465e0174d0f52b061d0c04d8aaeb3
LucasBA <lbassettaudain@ufl.edu>
2015-04-05 02:47:11 -0400

    Vision: Add VisServoing to setup

--- ros/ieee2015_vision/setup.py
+++ ros/ieee2015_vision/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['chess_vision', 'ros_image_tools', 'object_detection'],
+    packages=['chess_vision', 'ros_image_tools', 'object_detection', 'visual_servoing'],
     package_dir={'': 'src'},
 )
 
