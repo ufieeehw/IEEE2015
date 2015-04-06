d699eabb721ef531c5e9e5393c85ff9c8b288bcb
Christine Moore <christinemoore321@gmail.com>
2015-04-05 22:01:11 -0400

    objDetect: add draw param

--- ros/ieee2015_vision/src/object_detection/detect_card.py
+++ ros/ieee2015_vision/src/object_detection/detect_card.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def find_card(img, height):
+def find_card(img, height, draw):
     kernelg = np.ones((8,8), np.uint8)
     kernelD = np.ones((2, 2), np.uint8)
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -9,17 +9,36 @@ def find_card(img, height):
     gray = cv2.adaptiveThreshold(grayscale,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,37,15)
     gray = cv2.erode(gray, kernelg)
-
+    #gray = cv2.dilate(gray, kernelD)
+    
+    if draw == true:
+        cv2.imshow('adaptive thresh', gray)
+    
     contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    approx_height = (-838900 * height) + 239926
+    approx_height = (-486526 * height) + 163108
     sigma = 3000
      #using grayscale thresh
     goodContours = []
     for current in contours:
         area = cv2.contourArea(current)
-        if area > (approx_height - sigma) and area < (approx_height + sigma): #will need to be adjusted !!!!!
+        if area > 30000: #will need to be adjusted !!!!!
+            if draw == true:
+                print 'this is area'
+                print area
+                cv2.drawContours(img,[current],0,(0,255,0),1)
             goodContours.append(current)
+
+    #for cnt in goodContours:
+        #cv2.drawContours(img,[cnt],0,(0,255,0),1)
+
+    #cv2.imshow('contos', img);
+    #cv2.waitKey(0);
+
+      #squares = [] #goodContours[0] # create list of the tile squares
+      #for c in goodContours:
+        #squares.extend(c)
+        #squares.append(cv2.minAreaRect(c)) #get a rectangle around the contour
 
     boxpoints = cv2.minAreaRect(goodContours[0])
              # rect = ((center_x,center_y),(width,height),angle)
@@ -40,7 +59,16 @@ def find_card(img, height):
     centerX = (maxX + minX)/2
     centerY = (maxY + minY)/2
 
+    if draw == true:
+        cv2.drawContours(img, [points], 0, (0, 0, 255), 1)
+        cv2.imshow('contos', img);
+
     angle = boxpoints[2]
     
     return (centerX, centerY), angle
 
+    #img = cv2.imread('heights/rubix23.jpg')
+    #find_rubix(img)
+
+#colorimage = cv2.imread('ti/25hc.jpg')
+#find_card(colorimage, .255)
