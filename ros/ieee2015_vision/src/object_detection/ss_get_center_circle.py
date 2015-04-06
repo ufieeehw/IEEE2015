b142bd3a22daab90d1b745c90d02613fe2a69f7b
Christine Moore <christinemoore321@gmail.com>
2015-04-05 21:53:03 -0400

    objDetect: added draw parameter

--- ros/ieee2015_vision/src/object_detection/ss_get_center_circle.py
+++ ros/ieee2015_vision/src/object_detection/ss_get_center_circle.py
@@ -1,33 +1,48 @@
 import cv2
 import numpy as np
-import ss_get_axis_points
 
 
-def get_center_circle(img, points):
+def get_center_circle(img, points, draw):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    #print 'and we made it here'
     ret,thresh3 = cv2.threshold(gray,250,255,cv2.THRESH_TRUNC)
     thresh = cv2.adaptiveThreshold(thresh3, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                     cv2.THRESH_BINARY, 23, 3)
 
+    if draw == true:
+        cv2.imshow('thresh', thresh)
+    
     #seems to be good for circles
-    circles = cv2.HoughCircles(thresh3, cv2.cv.CV_HOUGH_GRADIENT, 1, 200, 100, 100, 100, 50)
+    circles = cv2.HoughCircles(thresh3, cv2.cv.CV_HOUGH_GRADIENT, 10, 200, 100, 100, 100, 50)
     #circles = cv2.HoughCircles(thresh3, cv2.cv.CV_HOUGH_GRADIENT, 1, 200, 100, 100, 100, 50)
+
+    if draw == true:
+        cv2.imshow('circles', img)
+        cv2.waitKey(0)
+
     goodcircle = []
-    radius_center = (-482 * height) + 197
-    sigma = 8
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0,:]:
             temppoint = (i[0], i[1])
             tempans = cv2.pointPolygonTest(points, temppoint, False)
             # draw the outer circle
-            if tempans >= 0 and i[2] > radius_center - sigma and i[2] < radius_center + sigma:
+            if tempans >= 0 :
+                if draw == true:
+                    print 'this is circle radius'
+                    print i[2]
+                    cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                    cv2.circle(img, (i[0], i[1]), i[2], (0, 0, 255), 10)
+                    cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 10)
                 goodcircle.append(i[0])
                 goodcircle.append(i[1])
-    else:
-        print "You screwed up"
 
     return goodcircle
+
+#img = cv2.imread('ti/15hss.jpg')
+#get_center_circle(img, 5)
+
+
 
 
--- ros/ieee2015_vision/src/object_detection/ss_get_lit_button.py
+++ ros/ieee2015_vision/src/object_detection/ss_get_lit_button.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 
 
-def get_lit_button(img):
-    #img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-    #below ranges should work for all of the lights on ranges
+def get_lit_button(img, draw):
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+   
     lower_on = np.array([0, 80, 140])
     upper_on = np.array([255, 255, 255])
 
-    bright_button = cv2.inRange(img, lower_on, upper_on)
+    bright_button = cv2.inRange(hsv_img, lower_on, upper_on)
 
     kernel = np.ones((8, 8), np.uint8)
     #kernel for eroding
@@ -18,20 +18,16 @@ def get_lit_button(img):
     eroded = cv2.erode(bright_button, kernel2)
     dilated = cv2.dilate(eroded, kernel3)
     closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel, iterations=10)
+    
+    if draw == true:
+        cv2.imshow('closing', closing)
 
     ############################################Start of Contour Manipulation######################
     #get minimum contour
     contours, hierarchy = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    #filtering contours to find the green and blue button
-    best_ctn = []
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 8000:
-            best_ctn.append(cnt)
-
     #best_ctn should ideally be length of one at this point
-    lit_button = best_ctn[0]
+    lit_button = contours[0]
 
     leftmost = tuple(lit_button[lit_button[:, :, 0].argmin()][0])
 
@@ -41,8 +37,25 @@ def get_lit_button(img):
 
     bottommost = tuple(lit_button[lit_button[:, :, 1].argmax()][0])
 
+    #Testing
+    if draw == true:
+        cv2.circle(img, (leftmost), 2, (0, 0, 255), 10)
+        cv2.circle(img, (rightmost), 2, (0, 0, 255), 10)
+        cv2.circle(img, (topmost), 2, (0, 0, 255), 10)
+        cv2.circle(img, (bottommost), 2, (0, 0, 255), 10)
+    
+
     mean_cols = int((leftmost[0] + rightmost[0]) / 2)
 
     mean_rows = int((topmost[1] + bottommost[1]) / 2)
    
-    return (mean_cols, mean_rows), closing
+    if draw == true:
+        cv2.circle(img, (mean_cols, mean_rows), 2, (0, 0, 0), 10)
+        cv2.imshow('points', img)
+
+    return (mean_cols, mean_rows)
+#img = cv2.imread('ti/ss3.JPG')
+#mg = cv2.resize(img,None,fx=.2, fy=.2,)
+#get_lit_button(img)
+#mc, mr, clsing = get_lit_button(img)
+#print mc, mr
