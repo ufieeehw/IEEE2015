b3719ada90a055f2fe34c9bc2ab52096c6cec4e9
Christine Moore <christinemoore321@gmail.com>
2015-04-05 22:20:48 -0400

    objdetect: add draw param

--- ros/ieee2015_vision/src/object_detection/ss_get_axis_points.py
+++ ros/ieee2015_vision/src/object_detection/ss_get_axis_points.py
@@ -1,17 +1,20 @@
 import cv2
 import numpy as np
-import ss_get_center_circle
+import ss_get_center_circle_test
 
 
 #read in the image, resize is just so that I can see on screen
-def get_axis_points(img, height):
+def get_axis_points(img, height, draw):
     #gray color needed for threshold
-    img2 = img.copy()
-
-    gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                     cv2.THRESH_BINARY, 37, 8)
+
+    #anytime we show an image copy it then show it
+    if draw == true:
+        fakeThresh = thresh.copy()
+        cv2.imshow('using adaptiveThreshold', fakeThresh)
 
     #flip black and white pixels
     thresh = (255 - thresh)
@@ -27,14 +30,31 @@ def get_axis_points(img, height):
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     approx_area = (-649593 * height) + 180070
-    sigma = 10000
+    sigma = 14000
+
+    if draw == true:
+        print 'this is approx_area'
+        print approx_area
     #finding contour for the toy
     wholeToy = []
     for bae2 in contours:
         area = cv2.contourArea(bae2)
+        if draw == true:
+            print 'this is all area'
+            print area
         if area > (approx_area - sigma) and area < (approx_area + sigma):
+            if draw == true:
+                print 'this is area'
+                print area
+                cv2.drawContours(img, [bae2], 0, (0, 255, 0), 10)
             wholeToy.append(bae2)
 
+    #test img
+    if draw == true:
+        tempimg = img.copy()
+        #tempimg = cv2.resize(tempimg, (0, 0), fx=0.2, fy=0.2)
+        cv2.imshow('contours', tempimg)
+       
     #first element should be only one and be the toy contour
     toyCnt = wholeToy[0]
 
@@ -44,7 +64,19 @@ def get_axis_points(img, height):
     points = cv2.cv.BoxPoints(boxpoints)         # Find four vertices of rectangle from above rect
     points = np.int0(np.around(points))
 
+    if draw == true:
+        cv2.drawContours(img, [points], 0, (0, 0, 255), 1)
+        cv2.imshow('detected box', img)
+    #cv2.waitKey(0)
+
     goodcircle = ss_get_center_circle_test.get_center_circle(img, points)
+    #PLOTS THE 4 CORNER POINTS OF THE RECTANGLE
+    #Testing
+    if draw == true:
+        cv2.circle(img, (points[0][0], points[0][1]), 2, (0, 0, 255), 10)
+        cv2.circle(img, (points[1][0], points[1][1]), 2, (0, 0, 255), 10)
+        cv2.circle(img, (points[2][0], points[2][1]), 2, (0, 0, 255), 10)
+        cv2.circle(img, (points[3][0], points[3][1]), 2, (0, 0, 255), 10)
 
     x = int(x)
     y = int(y)
@@ -52,14 +84,30 @@ def get_axis_points(img, height):
     ma = int(ma)
     MA = int(MA)
 
+    #angle = angle * -1 
+    if draw == true:
+        cv2.circle(img, (goodcircle[0], goodcircle[1]), 2, (0, 0, 255), 30)
+        imgf = img.copy()
+        #imgf = cv2.resize(imgf, (0, 0), fx=0.2, fy=0.2)
+        cv2.imshow('venterpoint', imgf)
+
     #gives us the dimensions so that we can form rotation matrix
-    #rows, cols, pages = img2.shape
+    rows, cols, pages = img.shape
+
+    #angle = angle + 100
     #positive angle goes counterclockwise
-    #to get rotated image
-    #M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
-    #dst = cv2.warpAffine(img2, M, (cols, rows))
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
+    dst = cv2.warpAffine(img, M, (cols, rows))
 
-    return angle, points, goodcircle#x, y, p1CentMajor, p2CentMajor, p1CentMinor, p2CentMinor, quarterMin1, quarterMin2, quarterMaj1, quarterMaj2
 
-#img = cv2.imread('test_functions/ti/25hss.jpg')
-#get_axis_points(img, .255)
+    #imgd = dst.copy()
+    #imgd = cv2.resize(imgd, (0, 0), fx=0.2, fy=0.2)
+    #cv2.imshow('rotated image', dst)
+    #cv2.waitKey(0)
+
+    if draw == true:
+        cv2.imshow('finla img', img)
+        cv2.waitKey(0)
+
+    return angle, points, goodcircle
+
