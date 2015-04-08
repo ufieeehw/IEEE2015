1c6d1d90828dc550dd1dd81bb5707422d518d57e
LucasBA <lbassettaudain@ufl.edu>
2015-04-08 16:29:36 -0400

    Vision: Fixed math on visual servoing server

--- ros/ieee2015_vision/src/visual_servoing/distance_server.py
+++ ros/ieee2015_vision/src/visual_servoing/distance_server.py
@@ -18,15 +18,26 @@ from geometry_msgs.msg import Point
 
 def handle_two_points(req):
     '''
-        Real_World_width = constant * distance_camera_to_object * Pixel_width
+        Real_World_width =  distance_camera_to_object * Pixel_width / constant 
 
     Calculate real world coordinates of desired camera location in the robot frame
-    (1.0/840) is a constant calculated from experimenting with the camera
+    8.79167 is a constant calculated from experimenting with the camera fogal length
     distance_camera_to_object = end_camera_height(in robot frame)+ 0.21658(vertical translation from course to robot)-req.point2.z(height of object)
     '''
-    xcomp1=((end_camera_x)+(1.0/840)*(end_camera_height+0.21658-req.point2.z)*(req.point2.x - req.point1.x))
-    ycomp1=((end_camera_y)+(1.0/840)*(end_camera_height+0.21658-req.point2.z)*(req.point2.y - req.point1.y))
-    dist1 = (xcomp1, ycomp1, 0.12) 
+    p=(req.point2.x - req.point1.x)*((end_camera_height-req.point2.z)/8.79167)
+    xcomp1=((end_camera_x)-(p/100))
+    #print "end_camera_height", end_camera_height
+    print "h*p/c" 
+    print "p", p/100
+    #xcomp1=((end_camera_x)-25*((req.point2.x + req.point1.x)/(10000000*(end_camera_height+0.18-req.point2.z))))
+    #xcomp1=((end_camera_x)+(1.0/840)*(end_camera_height+0.21658+0.15-req.point2.z)*(req.point2.y + req.point1.y))
+    print "end_camera_x", end_camera_x
+
+    ycomp1=-((end_camera_y)+(-req.point2.y + req.point1.y)*((end_camera_height-req.point2.z)/8.79167))/100
+    #ycomp1=((end_camera_y)+(1.0/840)*(end_camera_height+0.21658+0.15-req.point2.z)*(req.point2.x + req.point1.x))
+
+
+    dist1 = (xcomp1, ycomp1, 0.15) 
     dist2 = Point(*dist1)           #convert from list to Point msg
     #print "dist", dist
     #print "Returning vector components [%s , %s to %s , %s = %s]"%(req.point1.x, req.point1.y, req.point2.x, req.point2.y, (dist))
