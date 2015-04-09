589b49085a568821d6a1099fa0ccb621f607a309
IanVan <its_ian@me.com>
2015-04-08 22:24:09 -0400

    Path Planner: Generates unit direction vectors between critical corners

--- ros/ieee2015_localization/src/slam/pathing/path_planner.py
+++ ros/ieee2015_localization/src/slam/pathing/path_planner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
+import math
 from itertools import product
 from time import time
 from skfmm import distance
@@ -23,6 +24,7 @@ def plan_path(start_position, goal_position, image):
     img_ones[white_lines] = 0
 
     gdt_initial = distance(img_ones)
+    # allow leeway when following lines
     safe_space = distance(img_ones) > 10
 
     img_ones[safe_space] = 0
@@ -38,7 +40,7 @@ def plan_path(start_position, goal_position, image):
     gdt_final = distance(masked_array)
     
     pyplot.imshow(gdt_final, interpolation = 'nearest')
-    pyplot.show()
+    #pyplot.show()
     
     current = np.array(start_position)
 
@@ -71,17 +73,17 @@ def plan_path(start_position, goal_position, image):
         cv2.line(image, (_next[1], _next[0]), (current[1], current[0]), (0, 255, 0), 2)
 
     pyplot.imshow(image, interpolation = 'nearest')
-    pyplot.show()
+    #pyplot.show()
 
     return path
 
-def simplify_path(path):
+def get_corners(path):
     corners_all = []
     for i in range(len(path)-2):  
         if (slope(path[i], path[i+1]) != slope(path[i+1], path[i+2])):            
             corners_all.append(path[i])
-    print "ALL CORNERS:"
-    print corners_all
+    #print "ALL CORNERS:"
+    #print corners_all
     
     corners_simplified = []
     for i in range(len(corners_all)):
@@ -91,14 +93,37 @@ def simplify_path(path):
             (np.abs(c1[0] - c2[0]) > 5)  and (np.abs(c1[1] - c2[1]) < 2) or
             (np.abs(c1[0] - c2[0]) > 1)  and (np.abs(c1[1] - c2[1]) > 1)):
                 corners_simplified.append(corners_all[i])
-    print "NECESSARY CORNERS:"
-    print corners_simplified
+    #print "NECESSARY CORNERS:"
+    #print corners_simplified
+    return corners_simplified
     
 def slope((x1, y1), (x2, y2)):
     if (x1 == x2):
     	return 2
     else:
     	return (y2 - y1) / (x2 - x1)
+
+def get_direction_vectors(corners):
+    direction_vectors = []
+
+    for i in range(len(corners)-1):
+        p1 = corners[i]
+        p2 = corners[i+1]
+        #print "Start Point:", p1, 
+        #print "; End Point:", p2
+
+        # get vector distance between two consecutive points
+        distance = [p2[0]-p1[0], p2[1]-p1[1]]
+        #print "Distance Vector:", distance
+
+        # normalize distance vector to unit vector
+        norm = math.sqrt(distance[0]**2 + distance[1]**2)
+        direction = [distance[0] / norm, distance[1] / norm]
+        #print "Direction Unit Vector:", direction
+        #print "--------------------------------------------------------------------"
+        direction_vectors.append(direction)
+    return direction_vectors
+
 
 def main():
     # define path to image
@@ -113,7 +138,11 @@ def main():
     
     tic = time()
     planned_path = plan_path(start, goal, img)
-    simplify_path(planned_path)
+    corners = get_corners(planned_path)
+    direction_vectors = get_direction_vectors(corners)
+    # print direction unit vectors
+    for i in range(len(direction_vectors)):
+            print direction_vectors[i]
     toc = time() - tic   
     print '{} secs'.format(toc)
     
