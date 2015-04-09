2b83b24554c4ad47c0d2fc37ecc93b4dfb479ed1
Christine Moore <christinemoore321@gmail.com>
2015-04-08 23:57:51 -0400

    ObjDetect: update for testing

--- ros/ieee2015_vision/tests/test_vision.py
+++ ros/ieee2015_vision/tests/test_vision.py
@@ -6,6 +6,7 @@ from object_detection import etchaSketch_detect
 from object_detection import ss_get_axis_points
 from object_detection import ss_get_lit_button
 from ros_image_tools import Image_Subscriber
+from object_detection import ss_get_center_circle
 import cv2
 
 '''How to use this:
@@ -26,10 +27,12 @@ Remove: Uses of cv instead of cv2 (dependency issue)
 def image_callback(image):
     cv2.imshow("Input Image", image)
     # find_rubix(image, 0.3)
+    angle, points, good_circle = ss_get_axis_points.get_axis_points(img, .17, True)
+    ss_get_center_circle.get_center_circle(img, points, True)
     cv2.waitKey(1)
 
 
 if __name__ == '__main__':
     rospy.init_node('test_vision')
-    imager = Image_Subscriber('/robot/base_camera/image_raw', image_callback)
+    imager = Image_Subscriber('/robot/arm_camera/image_raw', image_callback)
     rospy.spin()
