77dee979928a816be990ddcae010bd75b7ede213
Christine Moore <christinemoore321@gmail.com>
2015-04-10 03:34:38 -0400

    objDetect: find points front camera

---
+++ ros/ieee2015_vision/src/object_detection/find_obj_square.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def find_obj_square(img, draw):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	#range for the white squares
+	thresh = cv2.inRange(hsv,np.array((0,0, 160)),np.array((255,255,255)))
+	thresh = 255 - thresh
+	kernel = np.ones((8, 8), np.uint8)
+	#kernel for dilating
+	kernel2 = np.ones((8, 5), np.uint8)
+	thresh = cv2.dilate(thresh, kernel2)
+	thresh = cv2.erode(thresh, kernel)
+	
+	if draw is True:
+		cv2.imshow('thresh', thresh)
+
+
+	l, w, h = img.shape
+	area_of_image = l * w
+	black = np.zeros((l,w), np.uint8) 
+
+
+
+	thresh = 255 -thresh
+	closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+	contours, hierarchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	points = []
+	for cnt in contours:
+		(x, y), (MA, ma), angle = cv2.fitEllipse(cnt)
+		point = (int(x), int(y))
+		points.append(point)
+		if draw is True:
+			cv2.circle(img, point, 5, [0, 0, 255], -1)
+			cv2.drawContours(black, [cnt], 0, (255, 255, 255), 10)
+
+	if draw is True:
+		cv2.imshow('all contours', black)
+		cv2.imshow('center points', img)
+	
+	return points
