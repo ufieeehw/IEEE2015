37c6573965e1d21d0fdb131fc8fcc5ddb00a2e71
Nikita Goldfarb <nikitagoldfarb@gmail.com>
2015-04-09 23:52:26 -0400

    Vision: more 'IEEE' modification

--- ros/ieee2015_simulator/scripts/etch_publisher.py
+++ ros/ieee2015_simulator/scripts/etch_publisher.py
@@ -10,36 +10,38 @@ rospy.init_node('pub-test', anonymous=True)
 pub = rospy.Publisher('end_efffector_angles_sub', high_level)
 
 pi = np.pi
+legs = 1
+body = .2
 
 array = [[0,	   0],
-		[pi*3/4,   0],#right
+		[pi*3/4+legs,   0],#right
 		[pi*5/12,  0],#left
 		[pi*5/12, pi],#down
 		[0,		  pi],#left
-		[pi*3/2+.5,  pi],#right bottom E ###########
-		[pi,	  pi],#left
-		[pi,	pi*1/2],#up, first half E
-		[pi*3/2+.5,pi*1/2],#out first leg ##########
-		[pi,	pi*1/2],#in
-		[pi,		0],	#top of E
-		[pi*9/4+.5,	0], #top of second E ########
-		[pi*7/4,	0],	#back
-		[pi*7/4,pi*1/2],#down half 2 E
-		[pi*9/4+.5,pi*1/2],#out leg ################
-		[pi*7/4,pi*1/2],#back in
-		[pi*7/4,  pi],  #bottom E
-		[pi*3+.5,	  pi],	#right to bottom 3 E ####
-		[pi*5/2,  pi],	#back
-		[pi*5/2,pi*1/2],#up half 3 E
-		[pi*3+.5,	pi*1/2],#out leg ################
-		[pi*5/2,pi*1/2],#back in
-		[pi*5/2,	0],	#up top E
-		[pi*3+.5,		0]]	#END     ################
+		[pi*3/2+legs,  	   pi],#right bottom E ###########
+		[pi,	  		   pi],#left
+		[pi,		pi*1/2-body],#up, first half E @@@@@@@
+		[pi*3/2+legs,  pi*1/2],#out first leg ##########
+		[pi,		   pi*1/2],#in
+		[pi,				0],#top of E
+		[pi*9/4+legs,		0],#top of second E ########
+		[pi*7/4,			0],#back
+		[pi*7/4,  pi*1/2+body],#down half 2 E @@@@@@@@@@
+		[pi*9/4+legs,  pi*1/2],#out leg ################
+		[pi*7/4,	   pi*1/2],#back in
+		[pi*7/4,  		   pi],#bottom E
+		[pi*3+legs,	  	   pi],#right to bottom 3 E ####
+		[pi*5/2,  		   pi],#back
+		[pi*5/2,  pi*1/2-body],#up half 3 E @@@@@@@@@@@@@
+		[pi*3+legs,    pi*1/2],#out leg ################
+		[pi*5/2,	   pi*1/2],#back in
+		[pi*5/2,			0],#up top E
+		[pi*3+legs,			0]]#END     ################
 
 for i in range(len(array)):
 	pub.publish(high_level(large_radians=array[i][0]/5, small_radians=array[i][1]/5))
 	print array[i][0],array[i][1]
-	time.sleep(.25)
+	time.sleep(.5)
 
 
 rospy.spin()
