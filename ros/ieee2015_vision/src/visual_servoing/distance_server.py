8bd62efb7bd9c19a36c3d3d8986476e9410e162c
LucasBA <lbassettaudain@ufl.edu>
2015-04-11 01:41:16 -0400

    Vision: flip the tf frames to correct

--- ros/ieee2015_vision/src/visual_servoing/distance_server.py
+++ ros/ieee2015_vision/src/visual_servoing/distance_server.py
@@ -24,20 +24,36 @@ def handle_two_points(req):
     8.79167 is a constant calculated from experimenting with the camera fogal length
     distance_camera_to_object = end_camera_height(in robot frame)+ 0.21658(vertical translation from course to robot)-req.point2.z(height of object)
     '''
-    p=(req.point2.x - req.point1.x)*((end_camera_height-req.point2.z)/8.79167)
-    xcomp1=((end_camera_x)-(p/100))
+    
+    #command filter: handles pid range as well as minimum commands
+    #if (p < ((diffheight/2)*0.71428571428571))
+        #p=
+
+    diffheight = (end_camera_z-0)
+    p=(req.point2.x - req.point1.x)*(diffheight/8.79167)/10
+
+    #ycomp1=((end_camera_x-0.03442)-(p))
     #print "end_camera_height", end_camera_height
-    print "h*p/c" 
-    print "p", p/100
+    #print "h*p/c" 
+    #print "p", p/100
     #xcomp1=((end_camera_x)-25*((req.point2.x + req.point1.x)/(10000000*(end_camera_height+0.18-req.point2.z))))
     #xcomp1=((end_camera_x)+(1.0/840)*(end_camera_height+0.21658+0.15-req.point2.z)*(req.point2.y + req.point1.y))
-    print "end_camera_x", end_camera_x
-
-    ycomp1=-((end_camera_y)+(-req.point2.y + req.point1.y)*((end_camera_height-req.point2.z)/8.79167))/100
+    
+    ycomp1=0
+    d1= end_camera_z
+    print 'd1', d1
+    px=((480-req.point2.y - req.point1.y)*(d1-req.point2.z)/8.79167)/100*0.5
+    print 'px', px
+    print 'd1', d1 
+    print 'end_camera_x', end_camera_x
+    xcomp1=(+((end_camera_x)-px))
+    if (xcomp1>=0.31):
+        return 1
+    #xcomp1=0.034+((end_camera_x)+((-req.point2.y + req.point1.y)*((end_camera_height-req.point2.z)/8.79167)/50*0.2))
     #ycomp1=((end_camera_y)+(1.0/840)*(end_camera_height+0.21658+0.15-req.point2.z)*(req.point2.x + req.point1.x))
 
 
-    dist1 = (xcomp1, ycomp1, 0.15) 
+    dist1 = (xcomp1, ycomp1, 0.150) 
     dist2 = Point(*dist1)           #convert from list to Point msg
     #print "dist", dist
     #print "Returning vector components [%s , %s to %s , %s = %s]"%(req.point1.x, req.point1.y, req.point2.x, req.point2.y, (dist))
@@ -59,9 +75,7 @@ def compute_distance_server():
     #rospy.spin()
 
 if __name__== "__main__":
-    #global end_camera_height
-    #global end_camera_x
-    #global end_camera_y
+    
 
     compute_distance_server()
     rospy.init_node('ieee2015_tf_broadcaster2')#I think this is wrong
@@ -72,10 +86,10 @@ if __name__== "__main__":
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('/wrist_joint','/robot', rospy.Time(0)) #change to /end_camera frame 
+            (trans,rot) = listener.lookupTransform('/robot','/wrist_joint', rospy.Time(0)) #change to /end_camera frame 
             end_camera_x=trans[0]
             end_camera_y=trans[1]
-            end_camera_height = trans[2]
+            end_camera_z = trans[2]
             
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
